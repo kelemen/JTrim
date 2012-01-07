fbf16900d6a3257313f2410be88c1e27f6a2e218
Attila Kelemen <attila.kelemen85@gmail.com>
2012-01-08 00:02:36 +0100

    Documented: org.jtrim.collections (package-info)

---
+++ JTrim/src/main/java/org/jtrim/collections/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Defines collection interfaces, implementations and methods not provided
+ * by the {@code java.util} package.
+ * <P>
+ * The two most important interface of this package are
+ * {@link org.jtrim.collections.RefList} and
+ * {@link org.jtrim.collections.RefCollection}.
+ * <P>
+ * Also see the useful static helper methods in
+ * {@link org.jtrim.collections.ArraysEx} and
+ * {@link org.jtrim.collections.CollectionsEx}.
+ */
+package org.jtrim.collections;
