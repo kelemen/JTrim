7a10d047517dead0f78d1f330cc8556c4f0644e5
Attila Kelemen <attila.kelemen85@gmail.com>
2012-01-08 18:22:25 +0100

    Documented: AbortTaskRefusePolicy, SilentTaskRefusePolicy

--- JTrim/src/main/java/org/jtrim/concurrent/SilentTaskRefusePolicy.java
+++ JTrim/src/main/java/org/jtrim/concurrent/SilentTaskRefusePolicy.java
@@ -1,17 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.concurrent;
 
 /**
+ * A {@link TaskRefusePolicy} which silently ignores refused tasks.
+ * <P>
+ * Note that there is only a single instance of this class which can be accessed
+ * through {@link #INSTANCE}.
+ *
+ * <h3>Thread safety</h3>
+ * This class is completely safe to use by multiple threads concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this class are <I>synchronization transparent</I> and
+ * can be called from any context (e.g.: while holding a lock).
  *
  * @author Kelemen Attila
  */
 public enum SilentTaskRefusePolicy implements TaskRefusePolicy {
+    /**
+     * The one and only instance of {@code SilentTaskRefusePolicy}.
+     */
     INSTANCE;
 
+    /**
+     * This method does nothing and returns immediately to the caller.
+     *
+     * @param task this argument is ignored
+     */
     @Override
     public void refuseTask(Runnable task) {
     }
