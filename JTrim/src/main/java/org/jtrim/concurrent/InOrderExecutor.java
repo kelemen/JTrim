17d7388e7d5e54811f4c114553265f8c81a68a5e
Attila Kelemen <attila.kelemen85@gmail.com>
2012-01-10 00:23:02 +0100

    Fixed some bugs and documented: InOrderExecutor

--- JTrim/src/main/java/org/jtrim/concurrent/InOrderExecutor.java
+++ JTrim/src/main/java/org/jtrim/concurrent/InOrderExecutor.java
@@ -1,41 +1,97 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jtrim.concurrent;
 
 import java.util.concurrent.*;
+import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * Defines an executor which forwards task to a given executor and executes
+ * tasks without running them concurrently. The tasks will be executed in the
+ * order the they were submitted to the {@link #execute(Runnable) execute}
+ * method. Subsequent tasks trying to be executed while another one scheduled to
+ * this executor is running will be queued and be executed when the running task
+ * terminates.
+ * <P>
+ * This executor is useful to call tasks whose are not safe to be called
+ * concurrently. This executor will effectively serialize the calls as if
+ * all the tasks were executed by a single thread even if the underlying
+ * executor uses multiple threads to execute tasks.
+ * <P>
+ * Note that this implementation does not expect the tasks to be
+ * <I>synchronization transparent</I> but of course they cannot wait for each
+ * other. If a tasks executed by this executor submits a task to this same
+ * executor and waits for this newly submitted tasks, it will dead-lock always.
+ * This is because no other tasks may run concurrently with the already running
+ * tasks and therefore the newly submitted task has no chance to start.
+ * <P>
+ * <B>Warning</B>: Instances of this class use an internal queue for tasks
+ * yet to be executed and if tasks are submitted to executor faster than it
+ * can actually execute it will eventually cause the internal buffer to overflow
+ * and throw an {@link OutOfMemoryError}. This can occur even if the underlying
+ * executor does not execute tasks scheduled to them because tasks will be
+ * queued immediately by the {@code execute} method before actually executing
+ * the task.
  *
+ * <h3>Thread safety</h3>
+ * The methods of this class are safe to use by multiple threads concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this interface are not <I>synchronization transparent</I>.
+ *
+ * @see InOrderScheduledSyncExecutor
+ * @see UpgraderExecutor
  * @author Kelemen Attila
  */
 public final class InOrderExecutor implements Executor {
     private final Executor executor;
-    private final InOrderScheduledSyncExecutor syncExecutor;
+    private final DispatchTask dispatchTask;
+    private final TaskScheduler taskScheduler;
 
+    /**
+     * Creates a new {@code InOrderExecutor} with the given backing executor.
+     * All tasks will be executed in the context of this given executor.
+     *
+     * @param executor the executor to which tasks will be eventually forwarded
+     *   to. This argument cannot be {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified executor is
+     *   {@code null}
+     */
     public InOrderExecutor(Executor executor) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+
         this.executor = executor;
-        this.syncExecutor = new InOrderScheduledSyncExecutor();
+        this.taskScheduler = new TaskScheduler(SyncTaskExecutor.getSimpleExecutor());
+        this.dispatchTask = new DispatchTask(taskScheduler);
     }
 
+    /**
+     * Enqueues the submitted task and schedules a task to the underlying
+     * executor which will eventually execute the task and possibly other
+     * submitted tasks. Note that there is no telling which task submitted to
+     * the underlying executor will execute the given task only that it will
+     * be executed eventually.
+     *
+     * @param command the task to be executed by the backing executor. This
+     *   argument cannot be {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified task is {@code null}
+     */
     @Override
     public void execute(Runnable command) {
-        executor.execute(new SyncTask(syncExecutor, command));
+        taskScheduler.scheduleTask(command);
+        executor.execute(dispatchTask);
     }
 
-    private static class SyncTask implements Runnable {
-        private final Executor syncExecutor;
-        private final Runnable command;
+    private static class DispatchTask implements Runnable {
+        private final TaskScheduler taskScheduler;
 
-        public SyncTask(Executor syncExecutor, Runnable command) {
-            this.syncExecutor = syncExecutor;
-            this.command = command;
+        public DispatchTask(TaskScheduler taskScheduler) {
+            this.taskScheduler = taskScheduler;
         }
 
         @Override
         public void run() {
-            syncExecutor.execute(command);
+            taskScheduler.dispatchTasks();
         }
     }
 }
