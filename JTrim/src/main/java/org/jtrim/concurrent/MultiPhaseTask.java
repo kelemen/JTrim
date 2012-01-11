222cb1a66633bba1d89241f0580434cbc5fd649d
Attila Kelemen <attila.kelemen85@gmail.com>
2012-01-11 20:45:31 +0100

    Documented: MultiPhaseTask

--- JTrim/src/main/java/org/jtrim/concurrent/MultiPhaseTask.java
+++ JTrim/src/main/java/org/jtrim/concurrent/MultiPhaseTask.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jtrim.concurrent;
 
 import java.util.concurrent.*;
@@ -9,6 +5,26 @@ import java.util.concurrent.atomic.*;
 import org.jtrim.utils.*;
 
 /**
+ * Defines a task which is made of multiple subtasks. Once the task is finished
+ * anyone may call {@link #finishTask(Object, Throwable, boolean) finishTask}
+ * to finish this {@code MultiPhaseTask}. The main advantages of this class
+ * is that it offers a {@link #getFuture() future} object representing the
+ * {@code MultiPhaseTask} and it can notify a listener when the
+ * {@code MultiPhaseTask} terminates and no more subtasks may run anymore.
+ * <P>
+ * Therefore to use this class, submit subtasks by one of the
+ * {@code executeSubTask} or {@code submitSubTask} methods and when finished
+ * invoke the {@code finishTask} method (possibly from a subtask).
+ *
+ * <h3>Thread safety</h3>
+ * The methods of this class are safe to use by multiple threads concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this interface are not <I>synchronization transparent</I>.
+ *
+ * @param <ResultType> the type of the result of this task. The result can be
+ *   specified to the {@code finishTask} method and later can also be retrieved
+ *   by the shared {@link #getFuture() future's} get methods.
  *
  * @author Kelemen Attila
  */
@@ -18,13 +34,55 @@ public final class MultiPhaseTask<ResultType> {
     private final AtomicReference<FinishResult<ResultType>> finishResult;
     private final ExecutorService syncExecutor;
 
+    /**
+     * The listener interface which can be notified when a specific
+     * {@code MultiPhaseTask} terminates.
+     *
+     * <h3>Thread safety</h3>
+     * This interface doesn't need to be safe to use by multiple threads
+     * concurrently.
+     *
+     * <h4>Synchronization transparency</h4>
+     * The methods of this interface are not required to be
+     * <I>synchronization transparent</I>.
+     *
+     * @param <ResultType> the type of the result of the task this listener
+     *   receives notification from
+     */
     public static interface TerminateListener<ResultType> {
+        /**
+         * Invoked when the associated {@code MultiPhaseTask} terminates.
+         * {@code MultiPhaseTask} instances may call this method only once.
+         * When this method is called no more tasks scheduled to the
+         * {@code MultiPhaseTask} allowed to execute (even those scheduled
+         * before terminating the task).
+         *
+         * @param result the result of the associated {@code MultiPhaseTask}.
+         *   This argument can be {@code null}, if the result of the task is
+         *   {@code null}, or it did not terminate normally (i.e.: has been
+         *   canceled, caused an exception).
+         * @param exception the exception caused by the associated
+         *   {@code MultiPhaseTask} or {@code null} if no such error occurred.
+         *   Note that usually when this argument is non-null, the
+         *   {@code result} is {@code null} but it is not strictly required.
+         * @param canceled {@code true} if the associated {@code MultiPhaseTask}
+         *   was canceled, so it could not finish its intended task,
+         *   {@code false} otherwise
+         */
         public void onTerminate(ResultType result, Throwable exception,
             boolean canceled);
     }
 
+    /**
+     * Creates a new non-terminated {@code MultiPhaseTask} with the given
+     * terminate listener.
+     *
+     * @param terminateListener the listener to be notified when this task
+     *   terminates. This argument can be {@code null}, if no such notification
+     *   is required.
+     */
     public MultiPhaseTask(
-            final TerminateListener<? super ResultType> terminateListener) {
+            TerminateListener<? super ResultType> terminateListener) {
 
         this.finishResult = new AtomicReference<>(null);
         this.future = new MultiPhaseFuture();
@@ -39,11 +97,53 @@ public final class MultiPhaseTask<ResultType> {
         }
     }
 
+    /**
+     * Returns the {@code Future} representing this task. The returned future
+     * provides all the features of the {@code Future} interface. That is:
+     * <ul>
+     *  <li>
+     *   The {@code get} methods can be used to wait for this task to terminate
+     *   and retrieve its result.
+     *  </li>
+     *  <li>
+     *   Canceling the returned future will cancel this task with no exception
+     *   if it did not terminate for some other reason.
+     *  </li>
+     *  <li>
+     *   The {@code canceled} and the {@code done} state of this task can also
+     *   be queried.
+     *  </li>
+     * </ul>
+     *
+     * @return the {@code Future} representing this task. This method never
+     *   returns {@code null}.
+     */
     public Future<ResultType> getFuture() {
         return future;
     }
 
+    /**
+     * Submits the specified task as a subtask of this {@code MultiPhaseTask} to
+     * the given {@code UpdateTaskExecutor}. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the submitted subtask throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param executor the {@code UpdateTaskExecutor} to which the specified
+     *   task will be submitted to. This argument cannot be {@code null}.
+     * @param task the task to be executed on the specified
+     *   {@code UpdateTaskExecutor}. This argument cannot be {@code null}.
+     *
+     * @throws NullPointerException thrown if either {@code executor} or
+     *   {@code task} is {@code null}
+     */
     public void executeSubTask(final UpdateTaskExecutor executor, final Runnable task) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+        ExceptionHelper.checkNotNullArgument(task, "task");
+
         syncExecutor.execute(new Runnable() {
             @Override
             public void run() {
@@ -52,7 +152,28 @@ public final class MultiPhaseTask<ResultType> {
         });
     }
 
+    /**
+     * Submits the specified task as a subtask of this {@code MultiPhaseTask} to
+     * the given {@code Executor}. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the submitted subtask throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param executor the {@code Executor} to which the specified
+     *   task will be submitted to. This argument cannot be {@code null}.
+     * @param task the task to be executed on the specified
+     *   {@code Executor}. This argument cannot be {@code null}.
+     *
+     * @throws NullPointerException thrown if either {@code executor} or
+     *   {@code task} is {@code null}
+     */
     public void executeSubTask(final Executor executor, final Runnable task) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+        ExceptionHelper.checkNotNullArgument(task, "task");
+
         syncExecutor.execute(new Runnable() {
             @Override
             public void run() {
@@ -61,8 +182,31 @@ public final class MultiPhaseTask<ResultType> {
         });
     }
 
+    /**
+     * Submits the specified task as a subtask of this {@code MultiPhaseTask} to
+     * the given {@code ExecutorService}. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the submitted subtask throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param executor the {@code ExecutorService} to which the specified
+     *   task will be submitted to. This argument cannot be {@code null}.
+     * @param task the task to be executed on the specified
+     *   {@code ExecutorService}. This argument cannot be {@code null}.
+     * @return the future representing the submitted subtask. This method never
+     *   returns {@code null} assuming that the specified
+     *   {@code ExecutorService} never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if either {@code executor} or
+     *   {@code task} is {@code null}
+     */
     public Future<?> submitSubTask(final ExecutorService executor,
             final Runnable task) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+        ExceptionHelper.checkNotNullArgument(task, "task");
 
         final ObjectRef<Future<?>> result = new ObjectRef<>(null);
         syncExecutor.execute(new Runnable() {
@@ -75,8 +219,33 @@ public final class MultiPhaseTask<ResultType> {
         return result.getValue();
     }
 
+    /**
+     * Submits the specified task as a subtask of this {@code MultiPhaseTask} to
+     * the given {@code ExecutorService}. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the submitted subtask throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param <V> the type of the result of the submitted subtask
+     * @param executor the {@code ExecutorService} to which the specified
+     *   task will be submitted to. This argument cannot be {@code null}.
+     * @param task the task to be executed on the specified
+     *   {@code ExecutorService}. This argument cannot be {@code null}.
+     * @return the future representing the submitted subtask. This method never
+     *   returns {@code null} assuming that the specified
+     *   {@code ExecutorService} never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if either {@code executor} or
+     *   {@code task} is {@code null}
+     */
     public <V> Future<V> submitSubTask(final ExecutorService executor,
             final Callable<V> task) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+        ExceptionHelper.checkNotNullArgument(task, "task");
+
         final ObjectRef<Future<V>> result = new ObjectRef<>(null);
         syncExecutor.execute(new Runnable() {
             @Override
@@ -88,11 +257,50 @@ public final class MultiPhaseTask<ResultType> {
         return result.getValue();
     }
 
+    /**
+     * Executes the specified task as a subtask of this {@code MultiPhaseTask}
+     * synchronously on the calling thread. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the subtask to be executed throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param task the task to be executed. This argument cannot be
+     *   {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified task is {@code null}
+     */
     public void executeSubTask(final Runnable task) {
+        ExceptionHelper.checkNotNullArgument(task, "task");
+
         executeSubTask(new RunnableWrapper(task));
     }
 
+    /**
+     * Executes the specified task as a subtask of this {@code MultiPhaseTask}
+     * synchronously on the calling thread. The submitted task will not be
+     * actually executed if this {@code MultiPhaseTask} had been terminated
+     * before it is actually scheduled.
+     * <P>
+     * If the subtask to be executed throws an exception, this
+     * {@code MultiPhaseTask} will immediately be terminated with the given
+     * exception (non-canceled and {@code null} as a result).
+     *
+     * @param <V> the type of the result of the subtask to be executed
+     * @param task the task to be executed. This argument cannot be
+     *   {@code null}.
+     * @return the return value of the subtask to be executed or {@code null}
+     *   if the task could not be executed because this {@code MultiPhaseTask}
+     *   had already been terminated. Note that this method may also return
+     *   {@code null} if the subtask returns {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified task is {@code null}
+     */
     public <V> V executeSubTask(final Callable<V> task) {
+        ExceptionHelper.checkNotNullArgument(task, "task");
+
         final ObjectRef<V> result = new ObjectRef<>(null);
         syncExecutor.execute(new Runnable() {
             @Override
@@ -106,10 +314,8 @@ public final class MultiPhaseTask<ResultType> {
 
     // This method must be called in the context of the "syncExecutor"
     private <V> V executeSubTaskAlways(Callable<V> task) {
-        ExceptionHelper.checkNotNullArgument(task, "task");
-
         try {
-            return task != null ? task.call() : null;
+            return task.call();
         } catch (InterruptedException ex) {
             finishTask(null, ex, false);
             Thread.currentThread().interrupt();
@@ -120,6 +326,29 @@ public final class MultiPhaseTask<ResultType> {
         }
     }
 
+    /**
+     * Invoking this method causes this {@code MultiPhaseTask} to terminate.
+     * How this task is terminated depends on the arguments of this method.
+     * Note that this {@code MultiPhaseTask} may not be able to immediately
+     * terminate because some subtask maybe running concurrently but will
+     * terminate when those concurrently running tasks actually terminate.
+     * This method guarantees that subtask submitted after this method call
+     * will never execute. Note however that even subtasks submitted before this
+     * call may be prevented from executing.
+     *
+     * @param result the result of this task. This argument can also be
+     *   {@code null} if the result cannot be determined (e.g.: because the task
+     *   was canceled).
+     * @param exception the exception preventing this task to be completing
+     *   normally. Specifying {@code null} usually means that the result
+     *   is {@code null} as it could not be computed. However it is not required
+     *   and some intermediate result can be published with the exception. In
+     *   case this task completed normally, this argument can be {@code null}.
+     * @param canceled {@code true} if this task was canceled and does not need
+     *   to complete, {@code false} otherwise.
+     * @return {@code true} if this {@code MultiPhaseTask} did terminate before
+     *   this method returned, {@code false} if it will terminate later
+     */
     public boolean finishTask(
             ResultType result,
             Throwable exception,
@@ -136,10 +365,24 @@ public final class MultiPhaseTask<ResultType> {
         return syncExecutor.isTerminated();
     }
 
+    /**
+     * Terminates this task with a {@code canceled} state. This method call is
+     * equivalent to calling {@code getFuture().cancel(true)} and
+     * {@code finishTask(null, null, true}.
+     */
     public void cancel() {
         future.cancel(true);
     }
 
+    /**
+     * Checks whether this {@code MultiPhaseTask} has been terminated or not.
+     * In case this task is terminated: No more previously submitted subtask
+     * will execute (not even concurrently) and submitted subtasks will not
+     * be executed as well.
+     *
+     * @return {@code true} if this {@code MultiPhaseTask} has been terminated,
+     *   {@code false} otherwise
+     */
     public boolean isDone() {
         return future.isDone();
     }
@@ -191,7 +434,9 @@ public final class MultiPhaseTask<ResultType> {
         public ResultType get(long timeout, TimeUnit unit) throws
                 InterruptedException, ExecutionException, TimeoutException {
 
-            syncExecutor.awaitTermination(timeout, unit);
+            if (!syncExecutor.awaitTermination(timeout, unit)) {
+                throw new TimeoutException();
+            }
             return fetchResult();
         }
     }
