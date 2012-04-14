8eee7061808237798f4941f339ac53aff4ecf904
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-14 14:46:42 +0200

    - Documented CopyOnTriggerEventHandlerContainer

--- JTrim/src/main/java/org/jtrim/event/CopyOnTriggerEventHandlerContainer.java
+++ JTrim/src/main/java/org/jtrim/event/CopyOnTriggerEventHandlerContainer.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.event;
 
 import java.util.ArrayList;
@@ -17,6 +12,33 @@ import org.jtrim.collections.RefList;
 import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * An {@link EventHandlerContainer} implementation which creates a copy of the
+ * currently registered listeners when dispatching events. That is, in the
+ * {@link #onEvent(EventDispatcher, java.lang.Object) onEvent} method, it first
+ * creates copy of the currently added listeners as done by the
+ * {@link #getListeners() getListeners()} method and then dispatches the event
+ * to all of them.
+ * <P>
+ * This implementation allows to register the same listener multiple times and
+ * listeners registered multiple times will be notified multiple times as well
+ * when dispatching events.
+ * <P>
+ * Adding and removing listeners are constant time operations but, of course,
+ * dispatching events requires linear time in the number of registered
+ * listeners (plus the time the listeners need).
+ *
+ * <h3>Thread safety</h3>
+ * As required by {@code EventHandlerContainer}, the methods of this class are
+ * safe to be accessed concurrently by multiple threads.
+ *
+ * <h4>Synchronization transparency</h4>
+ * As required by {@code EventHandlerContainer}, except for the {@code onEvent}
+ * method, methods of this class are <I>synchronization transparent</I>.
+ *
+ * @param <ListenerType> the type of the event handlers can possibly be added
+ *   to the container
+ * @param <ArgType> the type of the argument which can be passed to event
+ *   handlers by the {@code onEvent} method
  *
  * @author Kelemen Attila
  */
@@ -28,6 +50,10 @@ implements
     private final Lock writeLock;
     private final RefList<ListenerType> listeners;
 
+    /**
+     * Creates a new {@code CopyOnTriggerEventHandlerContainer} with no
+     * listeners registered.
+     */
     public CopyOnTriggerEventHandlerContainer() {
         ReadWriteLock listenerLock = new ReentrantReadWriteLock();
         this.readLock = listenerLock.readLock();
@@ -36,6 +62,12 @@ implements
         this.listeners = new RefLinkedList<>();
     }
 
+    /**
+     * {@inheritDoc }
+     * <P>
+     * <B>Implementation note</B>: Adding and removing (using the returned
+     * reference) a listener is a constant time operation.
+     */
     @Override
     public ListenerRef<ListenerType> registerListener(ListenerType listener) {
         ExceptionHelper.checkNotNullArgument(listener, "listener");
@@ -50,6 +82,16 @@ implements
         return new CollectionBasedListenerRef<>(readLock, writeLock, listenerRef);
     }
 
+    /**
+     * {@inheritDoc }
+     * <P>
+     * <B>Implementation note</B>: In case an exception is thrown by a
+     * registered listener, other listeners will still be invoked. After
+     * notifying all the listeners, the first exception thrown by the listeners
+     * will be rethrown by this method suppressing all other exceptions (i.e.:
+     * adding them as {@link Throwable#addSuppressed(Throwable) suppressed}
+     * exceptions to the thrown exception).
+     */
     @Override
     public void onEvent(
             EventDispatcher<? super ListenerType, ? super ArgType> eventDispatcher,
@@ -76,6 +118,12 @@ implements
         }
     }
 
+    /**
+     * {@inheritDoc }
+     * <P>
+     * <B>Implementation note</B>: Retrieving the listeners is a linear time
+     * operation in the number of registered listeners.
+     */
     @Override
     public Collection<ListenerType> getListeners() {
         readLock.lock();
@@ -88,6 +136,12 @@ implements
         }
     }
 
+    /**
+     * {@inheritDoc }
+     * <P>
+     * <B>Implementation note</B>: Retrieving the number of listeners is a
+     * constant time operation.
+     */
     @Override
     public int getListenerCount() {
         readLock.lock();
