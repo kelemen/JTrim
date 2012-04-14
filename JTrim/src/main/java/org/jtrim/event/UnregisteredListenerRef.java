891ad240aa9ec735d113858038d2c0d1c1c70c52
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-14 19:40:48 +0200

    - Documented UnregisteredListenerRef

--- JTrim/src/main/java/org/jtrim/event/UnregisteredListenerRef.java
+++ JTrim/src/main/java/org/jtrim/event/UnregisteredListenerRef.java
@@ -1,10 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jtrim.event;
 
+import org.jtrim.utils.ExceptionHelper;
+
 /**
+ * A {@link ListenerRef} which defines a listener which is already unregistered
+ * and will not be notified of events.
+ *
+ * <h3>Thread safety</h3>
+ * Instances of this class are immutable and as such, safe to be
+ * accessed by multiple threads concurrently. Note however, that the referenced
+ * listener may not be an immutable object.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this class are <I>synchronization transparent</I>.
+ *
+ * @param <ListenerType> the type of the listener already unregistered listener
  *
  * @author Kelemen Attila
  */
@@ -14,19 +24,47 @@ implements
 
     private final ListenerType listener;
 
+    /**
+     * Initializes the {@code UnregisteredListenerRef} with the specified
+     * listener to be returned by the {@link #getListener() getListener()}
+     * method.
+     *
+     * @param listener the listener to be returned by the {@code #getListener()}
+     *   method. This argument cannot be {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified listener is
+     *   {@code null}
+     */
     public UnregisteredListenerRef(ListenerType listener) {
+        ExceptionHelper.checkNotNullArgument(listener, "listener");
+
         this.listener = listener;
     }
 
+    /**
+     * Returns {@code false}, since this listener is considered to be always
+     * unregistered.
+     *
+     * @return {@code false} always
+     */
     @Override
     public boolean isRegistered() {
         return false;
     }
 
+    /**
+     * This method does nothing and returns immediately to the caller.
+     */
     @Override
     public void unregister() {
     }
 
+    /**
+     * Returns the listener specified at construction time.
+     *
+     * @return the listener specified at construction time. This method never
+     *   returns {@code null}.
+     */
     @Override
     public ListenerType getListener() {
         return listener;
