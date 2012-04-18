4bffb030ea85ab86053de9f36298f117768f38b7
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-18 23:34:19 +0200

    Created package-info for the "org.jtrim.concurrent.async.io" package.

---
+++ jtrim-async/src/main/java/org/jtrim/concurrent/async/io/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes and interface to asynchronously read from channels and
+ * streams.
+ *
+ * @see org.jtrim.concurrent.async.io.AsyncChannelLink
+ */
+package org.jtrim.concurrent.async.io;
