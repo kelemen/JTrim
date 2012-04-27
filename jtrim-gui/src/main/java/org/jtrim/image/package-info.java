92c589b82cb4791ae7e8ca2546159558e61d4ce9
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-27 23:41:35 +0200

    Added package-info for the "org.jtrim.image" package.

---
+++ jtrim-gui/src/main/java/org/jtrim/image/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines classes for storing information about images. These classes are used
+ * by the {@code org.jtrim.image.async} package.
+ * <P>
+ * There are also some useful static helper methods in
+ * {@link org.jtrim.image.ImageData}.
+ */
+package org.jtrim.image;
