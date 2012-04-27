d615041f0a41e72ad1aa380d4e84da1fb65863da
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-27 23:30:34 +0200

    Documented ImageReceiveException

--- jtrim-gui/src/main/java/org/jtrim/image/ImageReceiveException.java
+++ jtrim-gui/src/main/java/org/jtrim/image/ImageReceiveException.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.image;
 
 import org.jtrim.concurrent.async.DataTransferException;
 
 /**
+ * Defines the cause of a failure of an image retrieval attempt.
  *
  * @author Kelemen Attila
  */
@@ -15,24 +11,47 @@ public class ImageReceiveException extends DataTransferException {
     private static final long serialVersionUID = -8807903975010525439L;
 
     /**
-     * Creates a new instance of <code>ImageReceiveException</code> without detail message.
+     * Creates the exception with {@code null} as its
+     * {@link #getMessage() detail message}.
      */
     public ImageReceiveException() {
     }
 
 
     /**
-     * Constructs an instance of <code>ImageReceiveException</code> with the specified detail message.
-     * @param msg the detail message.
+     * Creates the exception with a specific
+     * {@link #getMessage() detail message}.
+     *
+     * @param message the message to be returned by
+     *   {@link #getMessage() getMessage()}. This argument can be {@code null}.
      */
-    public ImageReceiveException(String msg) {
-        super(msg);
+    public ImageReceiveException(String message) {
+        super(message);
     }
 
+    /**
+     * Creates the exception with a specific {@link #getCause() cause}.
+     * The {@link #getMessage() detail message} will be the string returned
+     * by the {@code cause.toString()} call.
+     *
+     * @param cause the cause of the {@code ImageReceiveException}. This
+     *   exception can later be retrieved by the {@link #getCause() getCause()}
+     *   method. This argument can be {@code null} if there is no such cause.
+     */
     public ImageReceiveException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Creates the exception with a specific
+     * {@link #getMessage() detail message} and {@link #getCause() cause}.
+     *
+     * @param message the message to be returned by
+     *   {@link #getMessage() getMessage()}. This argument can be {@code null}.
+     * @param cause the cause of the {@code ImageReceiveException}. This
+     *   exception can later be retrieved by the {@link #getCause() getCause()}
+     *   method. This argument can be {@code null} if there is no such cause.
+     */
     public ImageReceiveException(String message, Throwable cause) {
         super(message, cause);
     }
