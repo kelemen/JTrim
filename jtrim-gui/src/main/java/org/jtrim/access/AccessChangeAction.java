3f9ee9f55adf054e37ac0056cfba82eef459a443
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-29 00:32:57 +0200

    Documented: RightGroupHandler, AccessChangeAction

--- jtrim-gui/src/main/java/org/jtrim/access/AccessChangeAction.java
+++ jtrim-gui/src/main/java/org/jtrim/access/AccessChangeAction.java
@@ -1,9 +1,46 @@
 package org.jtrim.access;
 
 /**
+ * The listener interface to be invoked when the availability of a right group
+ * changes. That is, when a group of rights become available or unavailable
+ * in an {@link AccessManager}.
+ * <P>
+ * {@code AccessChangeAction} implementations are intended to be added to a
+ * {@link RightGroupHandler} (which is an {@link AccessStateListener} of an
+ * {@code AccessManager}).
+ *
+ * <h3>Thread safety</h3>
+ * Most implementation of this interface are not required to be thread-safe
+ * but {@code AccessManager} managing the rights, may define otherwise.
+ *
+ * <h4>Synchronization transparency</h4>
+ * Implementations of this listener are not required to be
+ * <I>synchronization transparent</I> however they must not wait for
+ * asynchronous events.
+ *
+ * @see RightGroupHandler
  *
  * @author Kelemen Attila
  */
 public interface AccessChangeAction {
+    /**
+     * Invoked when a specific group of right availability changes. The group of
+     * rights are normally defined when the {@code AccessChangeAction} is added
+     * to a {@link RightGroupHandler}.
+     * <P>
+     * The {@code AccessManager} managing the group of rights is specified for
+     * this method. The {@code AccessManager} maybe used to determine what
+     * {@link AccessToken} instances block access to the right. Note however,
+     * that by the time this method gets called, the current rights available
+     * may change, and so this must be considered when the tokens are needed for
+     * some purpose. Regardless of this possible inconsistency when the
+     * {@code available} argument is {@code true}, it can be assumed that there
+     * are no blocking tokens.
+     *
+     * @param accessManager the {@code AccessManager} managing the rights in the
+     *   group of rights. This argument cannot be {@code null}.
+     * @param available {@code true} if the group of right, this
+     *   {@code AccessChangeAction} listens is available, {@code false} if not
+     */
     public void onChangeAccess(AccessManager<?, ?> accessManager, boolean available);
 }
--- jtrim-gui/src/main/java/org/jtrim/access/RightGroupHandler.java
+++ jtrim-gui/src/main/java/org/jtrim/access/RightGroupHandler.java
@@ -8,6 +8,31 @@ import org.jtrim.event.ListenerRef;
 import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * Defines an {@code AccessStateListener} which can keep track of the
+ * availability of a group of rights. The {@code RightGroupHandler} must be
+ * added to an {@code AccessManager} which notifies the
+ * {@code RightGroupHandler} of changes in the availability of rights.
+ * <P>
+ * To use this class add the instance of {@code RightGroupHandler} to an
+ * {@code AccessManager} (e.g.: for a {@link HierarchicalAccessManager}, it must
+ * be specified in the constructor) and then specify the right groups and
+ * the action to take when the group of right becomes available or unavailable
+ * by calling the {@link #addGroupListener(Collection, Collection, AccessChangeAction) addGroupListener}
+ * method.
+ *
+ * <h3>Thread safety</h3>
+ * Instances of {@code RightGroupHandler} are safe to be accessed from multiple
+ * threads concurrently but not however, that if {@code onEnterState} is called
+ * concurrently with another {@code onEnterState} of the same
+ * {@code RightGroupHandler} may cause the state of right availability to be
+ * impossible.
+ *
+ * <h4>Synchronization transparency</h4>
+ * Methods of this interface are not <I>synchronization transparent</I> but
+ * they do not wait for any external event and return relatively quickly.
+ *
+ * @param <RightType> the type of the rights which can be handled by the
+ *   {@code RightGroupHandler}
  *
  * @author Kelemen Attila
  */
@@ -16,13 +41,59 @@ implements
         AccessStateListener<RightType> {
 
     private final ReentrantLock mainLock;
+
+    // This maps rights to RightGroup instances which might be affected
+    // when the AccessState of the right (the key of the map) changes.
     private final Map<RightType, Set<RightGroup>> groups;
 
+    /**
+     * Creates a new {@code RightGroupHandler} which does not yet monitors the
+     * state of any right group.
+     *
+     * @param expectedGroupCount the maximum expected number of right groups to
+     *   be added to this {@code RightGroupHandler} concurrently. The value of
+     *   this argument does not affect correctness but may improve performance
+     *   of adding right groups. This argument must be greater than or equal to
+     *   zero.
+     *
+     * @throws IllegalArgumentException thrown if the specified
+     *   {@code expectedGroupCount} argument is a negative integer
+     */
     public RightGroupHandler(int expectedGroupCount) {
         this.mainLock = new ReentrantLock();
         this.groups = CollectionsEx.newHashMap(expectedGroupCount);
     }
 
+    /**
+     * Adds a group of rights which this {@code RightGroupHandler} must monitor
+     * for availability. Whenever the availability of the group of rights
+     * changes, the specified {@code AccessChangeAction} is invoked to take the
+     * appropriate action.
+     *
+     * @param readRights the {@code Collection} of rights for which read access
+     *   is required. Read access is granted to a particular right if there
+     *   is no conflicting write access. This argument can be {@code null} which
+     *   is equivalent to passing an empty {@code Collection}. The content of
+     *   this passed {@code Collection} is copied by this method and modifying
+     *   the {@code Collection} after this method has no effect on this
+     *   {@code RightGroupHandler}.
+     * @param writeRights the {@code Collection} of rights for which write
+     *   access is required. Read access is granted to a particular right if
+     *   there is no conflicting write or read access. This argument can be
+     *   {@code null} which is equivalent to passing an empty
+     *   {@code Collection}. The content of this passed {@code Collection} is
+     *   copied by this method and modifying the {@code Collection} after this
+     *   method has no effect on this {@code RightGroupHandler}.
+     * @param accessChangeAction the listener to be notified when the
+     *   availability of the specified rights changes. This argument cannot be
+     *   {@code null}.
+     * @return the {@code ListenerRef} which can be used to stop listening
+     *   for the currently specified group of rights. This method never returns
+     *   {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified
+     *   {@code accessChangeAction} is {@code null}
+     */
     public ListenerRef<AccessChangeAction> addGroupListener(
             Collection<? extends RightType> readRights,
             Collection<? extends RightType> writeRights,
@@ -52,6 +123,23 @@ implements
         };
     }
 
+    /**
+     * Checks whether any right group became available or unavailable and
+     * notifies the registered {@code AccessChangeAction} instances if so.
+     * <P>
+     * If an invoked {@link AccessChangeAction#onChangeAccess(AccessManager, boolean)}
+     * method throws an unchecked exception, it will be rethrown to the caller
+     * after all the {@code AccessChangeAction} listeners were notified. Only
+     * the first thrown exception will be rethrown, subsequent exceptions will
+     * be {@link Throwable#addSuppressed(Throwable) suppressed}.
+     *
+     * @param accessManager the {@code AccessManager} in which the given right
+     *   is managed. This argument cannot be {@code null}.
+     * @param right the right which has changed state. This argument can only be
+     *   {@code null} if the {@code AccessManager} supports {@code null} rights.
+     * @param state the new state which is true for the specified right
+     *   (or rights). This argument cannot be {@code null}.
+     */
     @Override
     public void onEnterState(
             AccessManager<?, RightType> accessManager,
@@ -69,8 +157,22 @@ implements
         }
 
         if (affectedGroups != null) {
+            Throwable toThrow = null;
             for (RightGroup group: affectedGroups) {
-                group.checkChanges(accessManager);
+                try {
+                    group.checkChanges(accessManager);
+                } catch (Throwable ex) {
+                    if (toThrow == null) {
+                        toThrow = ex;
+                    }
+                    else {
+                        toThrow.addSuppressed(ex);
+                    }
+                }
+            }
+
+            if (toThrow != null) {
+                ExceptionHelper.rethrow(toThrow);
             }
         }
     }
