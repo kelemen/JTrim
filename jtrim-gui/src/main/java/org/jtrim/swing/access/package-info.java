ccfcdcd3dc2414f502ee65b5da539ac21026892d
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-29 01:47:17 +0200

    Added package-info for the "org.jtrim.swing.access" package.

---
+++ jtrim-gui/src/main/java/org/jtrim/swing/access/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains classes and methods to help Swing GUIs using the
+ * {@link org.jtrim.access.AccessManager}. This includes automatically setting
+ * the state of components as rights become available and unavailable.
+ */
+package org.jtrim.swing.access;
