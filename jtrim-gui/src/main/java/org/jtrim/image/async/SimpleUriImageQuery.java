1d3fab93d476e3d52058c9f67f3e9ad66a9ee6a7
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-28 16:41:53 +0200

    Documented: SimpleUriImageQuery

--- jtrim-gui/src/main/java/org/jtrim/image/async/SimpleUriImageQuery.java
+++ jtrim-gui/src/main/java/org/jtrim/image/async/SimpleUriImageQuery.java
@@ -1,19 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.image.async;
 
 import java.net.URI;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import org.jtrim.concurrent.async.AsyncDataLink;
 import org.jtrim.concurrent.async.AsyncDataQuery;
 import org.jtrim.concurrent.async.AsyncFormatHelper;
 import org.jtrim.image.ImageData;
+import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * Defines an {@code AsyncDataQuery} which is able to retrieve images based on
+ * an {@code URI}. The {@code URI} is specified as an input for the query. To
+ * actually retrieve the image file from the external source, the {@link URL}
+ * class is used, therefore {@code SimpleUriImageLink} is able to retrieve any
+ * image which the {@code URL} class can. To load the image, the {@code ImageIO}
+ * library of Java is used and therefore the meta data of the retrieved image is
+ * a {@link JavaIIOMetaData}.
+ * <P>
+ * This implementation completely relies on the {@link SimpleUriImageLink} to
+ * retrieve images.
+ *
+ * <h3>Thread safety</h3>
+ * The methods of this class are safe to be accessed by multiple threads
+ * concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this class are not <I>synchronization transparent</I>.
+ *
+ * @see SimpleUriImageLink
  *
  * @author Kelemen Attila
  */
@@ -24,16 +38,54 @@ implements
     private final ExecutorService executor;
     private final long minUpdateTime; // nanoseconds
 
+    /**
+     * Creates the {@code SimpleUriImageQuery} with the given properties.
+     *
+     * @param executor the executor used to actually retrieve the image. That
+     *   is, the image is retrieved in a task submitted to this executor.
+     *   This argument cannot be {@code null}.
+     * @param minUpdateTime the minimum time in nanoseconds which must elapse
+     *   between providing partially complete images to the
+     *   {@code AsyncDataListener}. Note that to actually forward an image to
+     *   the {@code AsyncDataListener} requires the {@code BufferedImage} to be
+     *   copied (and while copying the loading of the image is suspended).
+     *   Therefore providing an intermediate image is a considerable overhead,
+     *   so it is important not to set this value too low. This argument must be
+     *   greater than or equal to zero.
+     *
+     * @throws NullPointerException thrown if the specified executor is
+     *   {@code null}
+     * @throws IllegalArgumentException thrown if the specified
+     *   {@code minUpdateTime} is less than zero
+     */
     public SimpleUriImageQuery(ExecutorService executor, long minUpdateTime) {
+        ExceptionHelper.checkNotNullArgument(executor, "executor");
+        ExceptionHelper.checkArgumentInRange(minUpdateTime, 0, Long.MAX_VALUE, "minUpdateTime");
+
         this.executor = executor;
         this.minUpdateTime = minUpdateTime;
     }
 
+    /**
+     * {@inheritDoc }
+     * <P>
+     * <B>Implementation note</B>: This implementation only creates and returns
+     * a new {@link SimpleUriImageLink} instance.
+     */
     @Override
-    public AsyncDataLink<ImageData> createDataLink(URI arg) {
+    public SimpleUriImageLink createDataLink(URI arg) {
         return new SimpleUriImageLink(arg, executor, minUpdateTime);
     }
 
+    /**
+     * Returns the string representation of this {@code AsyncDataQuery} in no
+     * particular format
+     * <P>
+     * This method is intended to be used for debugging only.
+     *
+     * @return the string representation of this object in no particular format.
+     *   This method never returns {@code null}.
+     */
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
