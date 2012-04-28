5f584a3e624cb407725f3db7092f2582c66db875
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-28 16:43:28 +0200

    Added package-info for the "org.jtrim.image.async" package.

---
+++ jtrim-gui/src/main/java/org/jtrim/image/async/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes for asynchronously retrieving images.
+ *
+ * @see org.jtrim.image.async.SimpleUriImageLink
+ * @see org.jtrim.image.async.SimpleUriImageQuery
+ */
+package org.jtrim.image.async;
