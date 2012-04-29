f7ca010ecb3f7978ae25c8c7a875ef7cb0f8ad1c
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-29 21:16:23 +0200

    Documented: InvisiblePanelFactory

--- jtrim-gui/src/main/java/org/jtrim/swing/access/InvisiblePanelFactory.java
+++ jtrim-gui/src/main/java/org/jtrim/swing/access/InvisiblePanelFactory.java
@@ -9,10 +9,27 @@ import javax.swing.JPanel;
 import org.jtrim.access.AccessManager;
 
 /**
+ * A {@link DecoratorPanelFactory} implementation creating invisible
+ * {@code JPanel} instances blocking all user inputs if install as a glass pane
+ * of a Swing component.
+ * <P>
+ * Note that this class is a singleton and its one and only instance can be
+ * accessed by {@link InvisiblePanelFactory#INSTANCE}.
+ *
+ * <h3>Thread safety</h3>
+ * Methods of this class are safe to be accessed from multiple threads
+ * concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The {@code createPanel} method of this class is not
+ * <I>synchronization transparent</I>.
  *
  * @author Kelemen Attila
  */
 public enum InvisiblePanelFactory implements DecoratorPanelFactory {
+    /**
+     * The one and only instance of {@code InvisiblePanelFactory}.
+     */
     INSTANCE;
 
     private static Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
@@ -24,6 +41,17 @@ public enum InvisiblePanelFactory implements DecoratorPanelFactory {
         component.addKeyListener(new KeyAdapter() {});
     }
 
+    /**
+     * Creates and returns a {@code JPanel} which has a completely transparent
+     * background and will block all user input from a component if installed
+     * as its glass pane.
+     *
+     * @param decorated this argument is ignored by this method
+     * @param accessManager this argument is ignored by this method
+     * @return a new {@code JPanel} which has a completely transparent
+     *   background and will block all user input from a component if installed
+     *   as its glass pane. This method never returns {@code null}.
+     */
     @Override
     public JPanel createPanel(Component decorated, AccessManager<?, ?> accessManager) {
         JPanel result = new JPanel();
