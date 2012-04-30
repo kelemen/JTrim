cfa68ad3e0060e75f60d5b1a758108bcbbb47775
Attila Kelemen <attila.kelemen85@gmail.com>
2012-05-01 00:22:36 +0200

    Documented: TaskCanceledException

--- jtrim-core/src/main/java/org/jtrim/concurrent/executor/TaskCanceledException.java
+++ jtrim-core/src/main/java/org/jtrim/concurrent/executor/TaskCanceledException.java
@@ -1,24 +1,62 @@
 package org.jtrim.concurrent.executor;
 
 /**
+ * Thrown by tasks when they detect that they were requested to be canceled.
+ *
+ * @see CancelableTask
+ * @see TaskExecutor
  *
  * @author Kelemen Attila
  */
 public class TaskCanceledException extends RuntimeException {
     private static final long serialVersionUID = 6512650623463624418L;
 
+    /**
+     * Creates the exception with a specific {@link #getCause() cause}.
+     * The {@link #getMessage() detail message} will be the string returned
+     * by the {@code cause.toString()} call.
+     *
+     * @param cause the causing exception of the {@code TaskCanceledException}.
+     *   The cause of {@code TaskCanceledException} should usually be
+     *   {@code null} but it is possible to specify the cause to preserve the
+     *   stack trace of other exceptions signaling cancellation. This argument
+     *   can be {@code null} if there is no cause.
+     */
     public TaskCanceledException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Creates the exception with a specific
+     * {@link #getMessage() detail message} and {@link #getCause() cause}.
+     *
+     * @param message the message to be returned by
+     *   {@link #getMessage() getMessage()}. This argument can be {@code null}.
+     * @param cause the causing exception of the {@code TaskCanceledException}.
+     *   The cause of {@code TaskCanceledException} should usually be
+     *   {@code null} but it is possible to specify the cause to preserve the
+     *   stack trace of other exceptions signaling cancellation. This argument
+     *   can be {@code null} if there is no cause.
+     */
     public TaskCanceledException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Creates the exception with a specific
+     * {@link #getMessage() detail message}.
+     *
+     * @param message the message to be returned by
+     *   {@link #getMessage() getMessage()}. This argument can be {@code null}.
+     */
     public TaskCanceledException(String message) {
         super(message);
     }
 
+    /**
+     * Creates the exception with {@code null} as its
+     * {@link #getMessage() detail message}.
+     */
     public TaskCanceledException() {
     }
 }
