28933007ff3083c86493e5ab9fcc649147f8e945
Attila Kelemen <attila.kelemen85@gmail.com>
2012-04-30 20:42:48 +0200

    Documented: CancelableTask

--- jtrim-core/src/main/java/org/jtrim/concurrent/executor/CancelableTask.java
+++ jtrim-core/src/main/java/org/jtrim/concurrent/executor/CancelableTask.java
@@ -1,9 +1,39 @@
 package org.jtrim.concurrent.executor;
 
 /**
+ * Defines a task which can be canceled through a {@link CancellationToken}.
+ * There is no constraint on what an implementation of {@code CancelableTask}
+ * may do, it must implement its sole {@link #execute(CancellationToken) execute}
+ * method do whatever task it wishes.
+ * <P>
+ * How a task must respond to cancellation requests is implementation dependent
+ * but it may always terminate by throwing a {@link TaskCanceledException}.
+ *
+ * <h3>Thread safety</h3>
+ * The thread-safety property of {@code CancelableTask} is completely
+ * implementation dependent, so in general they does not need to be thread-safe.
+ *
+ * <h4>Synchronization transparency</h4>
+ * {@code CancelableTask} is not required to be
+ * <I>synchronization transparent</I>.
+ *
+ * @see TaskExecutor
  *
  * @author Kelemen Attila
  */
 public interface CancelableTask {
-    public void run(CancellationToken cancelToken);
+    /**
+     * Executes the the implementation dependent task. The task may check
+     * periodically the specified {@code CancellationToken} to detect
+     * cancellation requests and return immediately or throw a
+     * {@link TaskCanceledException}.
+     *
+     * @param cancelToken the {@code CancellationToken} which can be checked
+     *   periodically by this task to detect cancellation requests. This
+     *   argument cannot be {@code null}.
+     *
+     * @throws TaskCanceledException thrown if the task detects that it was
+     *   canceled (usually by checking the provided {@code CancellationToken})
+     */
+    public void execute(CancellationToken cancelToken);
 }
