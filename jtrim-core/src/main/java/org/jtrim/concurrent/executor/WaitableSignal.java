d75ef643168e03acc8fa39f4b5a8c2b50ab6fd7d
Attila Kelemen <attila.kelemen85@gmail.com>
2012-05-08 22:58:30 +0200

    New class: WaitableSignal

---
+++ jtrim-core/src/main/java/org/jtrim/concurrent/executor/WaitableSignal.java
@@ -0,0 +1,77 @@
+package org.jtrim.concurrent.executor;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public final class WaitableSignal {
+    private final Lock lock;
+    private final Condition waitSignal;
+    private volatile boolean signaled;
+
+    public WaitableSignal() {
+        this.lock = new ReentrantLock();
+        this.waitSignal = lock.newCondition();
+        this.signaled = false;
+    }
+
+    public void signal() {
+        signaled = true;
+        lock.lock();
+        try {
+            waitSignal.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean isSignaled() {
+        return signaled;
+    }
+
+    public void waitSignal(CancellationToken cancelToken) {
+        if (signaled) {
+            return;
+        }
+
+        lock.lock();
+        try {
+            while (!signaled) {
+                CancelableWaits.await(cancelToken, waitSignal);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean waitSignal(CancellationToken cancelToken,
+            long timeout, TimeUnit timeUnit) {
+
+        if (signaled) {
+            return true;
+        }
+
+        long timeoutNanos = timeUnit.toNanos(timeout);
+        long startTime = System.nanoTime();
+        lock.lock();
+        try {
+            while (!signaled) {
+                long elapsed = System.nanoTime() - startTime;
+                long timeToWait = timeoutNanos - elapsed;
+                if (timeToWait <= 0) {
+                    return false;
+                }
+                CancelableWaits.await(cancelToken,
+                        timeToWait, TimeUnit.NANOSECONDS, waitSignal);
+            }
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
