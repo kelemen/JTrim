577ba509863dd983bc12de1f96fd928b0e5186ab
Attila Kelemen <attila.kelemen85@gmail.com>
2012-05-09 00:33:52 +0200

    Introduced DelegatedTaskExecutorService to be able to conveniently create an adjusted TaskExecutorService relying on any implementation.

---
+++ jtrim-core/src/main/java/org/jtrim/concurrent/executor/DelegatedTaskExecutorService.java
@@ -0,0 +1,106 @@
+package org.jtrim.concurrent.executor;
+
+import java.util.concurrent.TimeUnit;
+import org.jtrim.event.ListenerRef;
+import org.jtrim.utils.ExceptionHelper;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class DelegatedTaskExecutorService implements TaskExecutorService {
+    protected final TaskExecutorService wrappedExecutor;
+
+    public DelegatedTaskExecutorService(TaskExecutorService wrappedExecutor) {
+        ExceptionHelper.checkNotNullArgument(wrappedExecutor, "wrappedExecutor");
+        this.wrappedExecutor = wrappedExecutor;
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public TaskFuture<?> submit(
+            CancellationToken cancelToken,
+            CancelableTask task,
+            CleanupTask cleanupTask) {
+        return wrappedExecutor.submit(cancelToken, task, cleanupTask);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public <V> TaskFuture<V> submit(
+            CancellationToken cancelToken,
+            CancelableFunction<V> task,
+            CleanupTask cleanupTask) {
+        return wrappedExecutor.submit(cancelToken, task, cleanupTask);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void shutdown() {
+        wrappedExecutor.shutdown();
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void shutdownAndCancel() {
+        wrappedExecutor.shutdownAndCancel();
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public boolean isShutdown() {
+        return wrappedExecutor.isShutdown();
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public boolean isTerminated() {
+        return wrappedExecutor.isTerminated();
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public ListenerRef addTerminateListener(Runnable listener) {
+        return wrappedExecutor.addTerminateListener(listener);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void awaitTermination(CancellationToken cancelToken) {
+        wrappedExecutor.awaitTermination(cancelToken);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public boolean awaitTermination(
+            CancellationToken cancelToken, long timeout, TimeUnit unit) {
+
+        return wrappedExecutor.awaitTermination(cancelToken, timeout, unit);
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void execute(CancellationToken cancelToken, CancelableTask task, CleanupTask cleanupTask) {
+        wrappedExecutor.execute(cancelToken, task, cleanupTask);
+    }
+}
