d5bc2d186b476d599c6bb659382d9e5f193c06ea
Attila Kelemen <attila.kelemen85@gmail.com>
2012-06-28 18:59:59 +0200

    Added some additional test codes for ScheduledAccessToken.

--- jtrim-gui/src/test/java/org/jtrim/access/ScheduledAccessTokenTest.java
+++ jtrim-gui/src/test/java/org/jtrim/access/ScheduledAccessTokenTest.java
@@ -8,10 +8,9 @@ import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.jtrim.cancel.Cancellation;
 import org.jtrim.cancel.CancellationToken;
-import org.jtrim.concurrent.CancelableTask;
-import org.jtrim.concurrent.SyncTaskExecutor;
-import org.jtrim.concurrent.TaskExecutor;
+import org.jtrim.concurrent.*;
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 /**
@@ -39,14 +38,15 @@ public class ScheduledAccessTokenTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of createExecutor method, of class ScheduledAccessToken.
-     */
-    @Test
-    public void testNoBlocking() {
-        ScheduledAccessToken<String> token = new ScheduledAccessToken<>(
+    private static ScheduledAccessToken<String> createUnblockedToken() {
+        return new ScheduledAccessToken<>(
                 AccessTokens.createToken("INDEPENDENT"),
                 Collections.<AccessToken<String>>emptySet());
+    }
+
+    @Test
+    public void testNoBlocking() {
+        ScheduledAccessToken<String> token = createUnblockedToken();
 
         final AtomicInteger executed = new AtomicInteger(0);
         TaskExecutor executor = token.createExecutor(SyncTaskExecutor.getSimpleExecutor());
@@ -56,6 +56,74 @@ public class ScheduledAccessTokenTest {
                 executed.incrementAndGet();
             }
         }, null);
+        assertEquals(1, executed.get());
+    }
+
+    @Test
+    public void testNoBlockingCleanup() {
+        ScheduledAccessToken<String> token = createUnblockedToken();
+
+        final AtomicInteger executed = new AtomicInteger(0);
+        TaskExecutor executor = token.createExecutor(SyncTaskExecutor.getSimpleExecutor());
+        executor.execute(Cancellation.UNCANCELABLE_TOKEN, Tasks.noOpCancelableTask(),
+                new CleanupTask() {
+            @Override
+            public void cleanup(boolean canceled, Throwable error) {
+                assertFalse(canceled);
+                assertNull(error);
+
+                executed.incrementAndGet();
+            }
+        });
+        assertEquals(1, executed.get());
+    }
+
+    @Test
+    public void testReleaseEventSimplePostRelease() {
+        ScheduledAccessToken<String> token = createUnblockedToken();
+
+        final AtomicInteger executed = new AtomicInteger(0);
+        token.release();
+        token.addReleaseListener(new Runnable() {
+            @Override
+            public void run() {
+                executed.incrementAndGet();
+            }
+        });
+        assertEquals(1, executed.get());
+    }
+
+    @Test
+    public void testReleaseEventSimplePreRelease() {
+        ScheduledAccessToken<String> token = createUnblockedToken();
+
+        final AtomicInteger executed = new AtomicInteger(0);
+        token.addReleaseListener(new Runnable() {
+            @Override
+            public void run() {
+                executed.incrementAndGet();
+            }
+        });
+        token.release();
+        assertEquals(1, executed.get());
+    }
+
+    @Test
+    public void testReleaseEventWithTask() {
+        ScheduledAccessToken<String> token = createUnblockedToken();
+
+        final AtomicInteger executed = new AtomicInteger(0);
+        token.addReleaseListener(new Runnable() {
+            @Override
+            public void run() {
+                executed.incrementAndGet();
+            }
+        });
+
+        TaskExecutor executor = token.createExecutor(SyncTaskExecutor.getSimpleExecutor());
+        executor.execute(Cancellation.UNCANCELABLE_TOKEN, Tasks.noOpCancelableTask(), null);
+
+        token.release();
         assertEquals(1, executed.get());
     }
 }
