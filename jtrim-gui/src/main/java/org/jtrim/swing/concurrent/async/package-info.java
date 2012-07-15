351ec5cdcffd4cd532241bf8a9a29cf6aa09b03c
Attila Kelemen <attila.kelemen85@gmail.com>
2012-07-15 23:01:56 +0200

    Added package-info for the "org.jtrim.swing.concurrent.async" package.

---
+++ jtrim-gui/src/main/java/org/jtrim/swing/concurrent/async/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes and interfaces related to concurrent data retrieval in Swing
+ * applications.
+ *
+ * @see org.jtrim.swing.concurrent.async.BackgroundDataProvider
+ */
+package org.jtrim.swing.concurrent.async;
