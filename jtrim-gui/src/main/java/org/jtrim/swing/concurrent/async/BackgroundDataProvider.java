0871efa6b67274501147647d479d7d11aa67f659
Attila Kelemen <attila.kelemen85@gmail.com>
2012-07-15 22:20:19 +0200

    Documented BackgroundDataProvider

--- jtrim-gui/src/main/java/org/jtrim/swing/concurrent/async/BackgroundDataProvider.java
+++ jtrim-gui/src/main/java/org/jtrim/swing/concurrent/async/BackgroundDataProvider.java
@@ -22,24 +22,132 @@ import org.jtrim.swing.concurrent.SwingTaskExecutor;
 import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * Defines a class that allows to create {@code AsyncDataQuery} and
+ * {@code AsyncDataLink} instances which only provides data until a requested
+ * {@link AccessToken} is available.
+ * <P>
+ * Every {@code BackgroundDataProvider} has an associated {@link AccessManager}
+ * and data requested from a data query or data link created by the
+ * {@code BackgroundDataProvider} will be provided in the context of an
+ * access token of the associated access manager. That is, the
+ * {@link AsyncDataListener#onDataArrive(Object) onDataArrive(DataType)} method
+ * of the listeners will be called in the context of the access token.
+ *
+ * <h3>Thread safety</h3>
+ * Methods of this class are safe to be accessed from multiple threads
+ * concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * Methods of this class are <I>synchronization transparent</I> but they are
+ * non-blocking quick methods allowed to be called from the
+ * <I>AWT Event Dispatch Thread</I>.
+ *
+ * @param <IDType> the type of the request ID of the underlying access manager
+ * @param <RightType> the type of the rights handled by the underlying access
+ *   manager
  *
  * @author Kelemen Attila
  */
 public final class BackgroundDataProvider<IDType, RightType> {
     private final AccessManager<IDType, RightType> accessManager;
 
+    /**
+     * Creates a new {@code BackgroundDataProvider} with the given
+     * {@code AccessManager}.
+     *
+     * @param accessManager the {@code AccessManager} from which access tokens
+     *   are requested to transfer data in their context. This argument cannot
+     *   be {@code null}.
+     *
+     * @throws NullPointerException thrown if the specified access manager is
+     *   {@code null}
+     */
     public BackgroundDataProvider(AccessManager<IDType, RightType> accessManager) {
         ExceptionHelper.checkNotNullArgument(accessManager, "accessManager");
 
         this.accessManager = accessManager;
     }
 
+    /**
+     * Creates an {@code AsyncDataQuery} which forwards every requested to the
+     * specified {@code AsyncDataQuery} but delivers the retrieved data in the
+     * context of an access token of the access manager of this
+     * {@code BackgroundDataProvider}.
+     * <P>
+     * The returned data query will simply wrap the data links of the specified
+     * data query using the {@link #createLink(AccessRequest, AsyncDataLink) createLink}
+     * method. So everything what holds for the links created by the
+     * {@code createLink} holds for the data links of the returned data query.
+     *
+     * @param <QueryArgType> the type of the input of the query
+     * @param <DataType> the type of the data to be retrieved
+     * @param request the {@code AccessRequest} used to acquire the required
+     *   access token in which context the data will be provided. This argument
+     *   cannot be {@code null}.
+     * @param wrappedQuery the {@code AsyncDataQuery} actually used to retrieve
+     *   the data requested by the returned {@code AsyncDataQuery}. This
+     *   argument cannot be {@code null}.
+     * @return the {@code AsyncDataQuery} which forwards every requested to the
+     *   specified {@code AsyncDataQuery} but delivers the retrieved data in the
+     *   context of an access token of the access manager of this
+     *   {@code BackgroundDataProvider}. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     *
+     * @see #createLink(AccessRequest, AsyncDataLink)
+     */
     public <QueryArgType, DataType> AsyncDataQuery<QueryArgType, DataType> createQuery(
             AccessRequest<? extends IDType, ? extends RightType> request,
             AsyncDataQuery<QueryArgType, DataType> wrappedQuery) {
         return new SwingDataQuery<>(request, wrappedQuery);
     }
 
+    /**
+     * Creates an {@code AsyncDataLink} which forwards every requested to the
+     * specified {@code AsyncDataLink} but delivers the retrieved data in the
+     * context of an access token of the access manager of this
+     * {@code BackgroundDataProvider}.
+     * <P>
+     * The required access token is attempted to be retrieved every time the
+     * data is requested (i.e.: {@code getData} is invoked). In case the access
+     * token cannot be acquired, the data transferring is automatically
+     * canceled. If the access token can be acquired but the token is released
+     * while retrieving the data, the data retrieval process is canceled and no
+     * more data will be transfered to the listener. Note however that the
+     * wrapped data link may not respond to this cancellation request and may
+     * actually continue retrieving data but this retrieved data will not be
+     * forwarded to the listener of the data retrieval.
+     * <P>
+     * The listener is used the following way:
+     * <P>
+     * The {@code onDataArrive} method
+     * is always called in the context of the acquired access token (and
+     * therefore the access token will remain acquired at least until the
+     * {@code onDataArrive} method returns.
+     * <P>
+     * The {@code onDoneReceive} method is called after the acquired access
+     * token has been released. The access token can be acquired again in the
+     * {@code onDoneReceive} method successfully if other code did not acquire
+     * it.
+     *
+     * @param <DataType> the type of the data to be retrieved
+     * @param request the {@code AccessRequest} used to acquire the required
+     *   access token in which context the data will be provided. This argument
+     *   cannot be {@code null}.
+     * @param wrappedLink the {@code AsyncDataLink} actually used to retrieve
+     *   the data requested by the returned {@code AsyncDataLink}. This argument
+     *   cannot be {@code null}.
+     * @return the {@code AsyncDataLink} which forwards every requested to the
+     *   specified {@code AsyncDataLink} but delivers the retrieved data in the
+     *   context of an access token of the access manager of this
+     *   {@code BackgroundDataProvider}. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     *
+     * @see #createQuery(AccessRequest, AsyncDataQuery)
+     */
     public <DataType> AsyncDataLink<DataType> createLink(
             AccessRequest<? extends IDType, ? extends RightType> request,
             AsyncDataLink<DataType> wrappedLink) {
