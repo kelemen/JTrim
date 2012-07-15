a094950b3ea0c9229c4ebc34b348b8c226d93f1a
Attila Kelemen <attila.kelemen85@gmail.com>
2012-07-15 20:33:54 +0200

    Added package-info for the "org.jtrim.swing.concurrent" package.

---
+++ jtrim-gui/src/main/java/org/jtrim/swing/concurrent/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains classes and interface to help with executing tasks of Swing
+ * applications concurrently.
+ *
+ * @see org.jtrim.swing.concurrent.BackgroundTaskExecutor
+ * @see org.jtrim.swing.concurrent.SwingTaskExecutor
+ */
+package org.jtrim.swing.concurrent;
