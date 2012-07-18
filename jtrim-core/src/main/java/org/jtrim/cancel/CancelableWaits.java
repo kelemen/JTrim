7ec94e89426185ff5ffa991e6fabfd00c90b6a9b
Attila Kelemen <attila.kelemen85@gmail.com>
2012-07-18 20:54:29 +0200

    Introduced Cancellation.doAsCancelable to convert interrupt based cancellation to the cancellation mechanism used in JTrim.

--- jtrim-core/src/main/java/org/jtrim/cancel/CancelableWaits.java
+++ jtrim-core/src/main/java/org/jtrim/cancel/CancelableWaits.java
@@ -511,40 +511,6 @@ public final class CancelableWaits {
         }
     }
 
-    private static class ThreadInterrupter implements Runnable {
-        private final Lock mainLock;
-        private Thread thread;
-
-        public ThreadInterrupter(Thread thread) {
-            assert thread != null;
-
-            this.mainLock = new ReentrantLock();
-            this.thread = thread;
-        }
-
-        public void stopInterrupt() {
-            mainLock.lock();
-            try {
-                thread = null;
-            } finally {
-                mainLock.unlock();
-            }
-        }
-
-        @Override
-        public void run() {
-            mainLock.lock();
-            try {
-                Thread currentThread = thread;
-                if (currentThread != null) {
-                    currentThread.interrupt();
-                }
-            } finally {
-                mainLock.unlock();
-            }
-        }
-    }
-
     private static class BooleanRef {
         public boolean value;
     }
