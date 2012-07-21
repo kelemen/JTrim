58249480665e59350a0fd50271d80acc73e86748
Attila Kelemen <attila.kelemen85@gmail.com>
2012-07-21 16:54:38 +0200

    Added tests for GenericAsyncRenderer

---
+++ jtrim-gui/src/test/java/org/jtrim/swing/concurrent/async/GenericAsyncRendererTest.java
@@ -0,0 +1,347 @@
+package org.jtrim.swing.concurrent.async;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.jtrim.cancel.Cancellation;
+import org.jtrim.cancel.CancellationToken;
+import org.jtrim.concurrent.CancelableTask;
+import org.jtrim.concurrent.CleanupTask;
+import org.jtrim.concurrent.SyncTaskExecutor;
+import org.jtrim.concurrent.TaskExecutor;
+import org.jtrim.concurrent.TaskExecutorService;
+import org.jtrim.concurrent.ThreadPoolTaskExecutor;
+import org.jtrim.concurrent.async.AsyncDataController;
+import org.jtrim.concurrent.async.AsyncDataLink;
+import org.jtrim.concurrent.async.AsyncDataListener;
+import org.jtrim.concurrent.async.AsyncDataState;
+import org.jtrim.concurrent.async.AsyncReport;
+import org.jtrim.concurrent.async.SimpleDataState;
+import org.jtrim.utils.ExceptionHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class GenericAsyncRendererTest {
+
+    public GenericAsyncRendererTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private static <DataType> TestRenderer<DataType> createRenderer() {
+        return createRenderer(null);
+    }
+
+    private static <DataType> TestRenderer<DataType> createRenderer(DataRenderer<DataType> renderer) {
+        return new TestRenderer<>(renderer, 5000, TimeUnit.MILLISECONDS);
+    }
+
+    private static TaskExecutorService createParallelExecutor(int maxThreadCount) {
+        return new ThreadPoolTaskExecutor("Test Executor", maxThreadCount);
+    }
+
+    private static List<Integer> createTestDatas(int dataCount) {
+        List<Integer> datas = new ArrayList<>(dataCount);
+        for (int i = 0; i < dataCount; i++) {
+            datas.add(i);
+        }
+        return datas;
+    }
+
+    private static boolean findElement(Iterator<?> itr, Object element) {
+        while (itr.hasNext()) {
+            Object current = itr.next();
+            if (element == current) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void checkReceivedDatas(
+            List<?> expected,
+            List<?> received) {
+
+        Iterator<?> expectedItr = expected.iterator();
+        for (Object current: received) {
+            if (!findElement(expectedItr, current)) {
+                fail("Invalid received datas. "
+                        + "Expected = " + expected
+                        + ", Received = " + received);
+            }
+        }
+    }
+
+    private static void checkNotCanceledReceivedDatas(
+            List<?> expected,
+            List<?> received) {
+        checkReceivedDatas(expected, received);
+        if (expected.isEmpty() && received.isEmpty()) {
+            return;
+        }
+
+        Object lastExpected = expected.get(expected.size() - 1);
+        Object lastReceived = received.get(received.size() - 1);
+        assertSame("The final received data must be the final expected data.", lastExpected, lastReceived);
+    }
+
+    private void checkCommonErrors(TestRenderer<?> renderer) {
+        List<String> miscErrors = renderer.getMiscErrors();
+        if (!miscErrors.isEmpty()) {
+            fail(miscErrors.toString());
+        }
+
+        if (renderer.isStartRenderingCalled()) {
+            assertTrue("Missing finishRendering call.", renderer.isFinishRenderingCalled());
+        }
+    }
+
+    private void doTestWithoutData(TaskExecutor executor) {
+        AsyncRenderer asyncRenderer = new GenericAsyncRenderer(executor);
+
+        TestRenderer<Object> renderer = createRenderer();
+        renderer.allowAll();
+
+        RenderingState state = asyncRenderer.render(
+                Boolean.TRUE,
+                Cancellation.UNCANCELABLE_TOKEN,
+                null,
+                renderer);
+        AsyncReport report = renderer.awaitDone();
+
+        assertTrue("Rendering state is expected to be completed.", state.isRenderingFinished());
+        assertTrue("Rendering time is required to be non-negative", state.getRenderingTime(TimeUnit.NANOSECONDS) >= 0);
+        assertNotNull(report);
+        assertEquals(true, report.isSuccess());
+        assertEquals("Unexpected data", 0, renderer.getReceivedDatas().size());
+        checkCommonErrors(renderer);
+    }
+
+    private void doTestWithData(TaskExecutor executor, TaskExecutor dataExecutor, int dataCount) {
+        AsyncRenderer asyncRenderer = new GenericAsyncRenderer(executor);
+        List<Integer> datas = createTestDatas(dataCount);
+        AsyncDataLink<Integer> dataLink = new TestDataLink<>(dataExecutor, datas);
+
+        TestRenderer<Integer> renderer = createRenderer();
+        renderer.allowAll();
+
+        RenderingState state = asyncRenderer.render(
+                Boolean.TRUE,
+                Cancellation.UNCANCELABLE_TOKEN,
+                dataLink,
+                renderer);
+        AsyncReport report = renderer.awaitDone();
+
+        assertTrue("Rendering state is expected to be completed.", state.isRenderingFinished());
+        assertTrue("Rendering time is required to be non-negative", state.getRenderingTime(TimeUnit.NANOSECONDS) >= 0);
+        assertNotNull(report);
+        assertEquals(true, report.isSuccess());
+        checkNotCanceledReceivedDatas(datas, renderer.getReceivedDatas());
+        checkCommonErrors(renderer);
+    }
+
+    @Test
+    public void testSyncWithoutData() {
+        doTestWithoutData(SyncTaskExecutor.getSimpleExecutor());
+    }
+
+    @Test
+    public void testWithoutData() {
+        TaskExecutorService executor = createParallelExecutor(1);
+        try {
+            doTestWithoutData(executor);
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    @Test
+    public void testSyncWithData() {
+        doTestWithData(SyncTaskExecutor.getSimpleExecutor(), SyncTaskExecutor.getSimpleExecutor(), 5);
+    }
+
+    @Test
+    public void testWithData() {
+        TaskExecutorService executor = createParallelExecutor(2);
+        try {
+            doTestWithData(executor, executor, 10);
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    @Test
+    public void testOverwriteRender() {
+        TaskExecutorService executor = createParallelExecutor(2);
+        try {
+            Object renderingKey = Boolean.TRUE;
+
+            AsyncRenderer asyncRenderer = new GenericAsyncRenderer(executor);
+
+            AtomicInteger currentIndex = new AtomicInteger(0);
+            Queue<String> orderErrors = new ConcurrentLinkedQueue<>();
+
+            TestRenderer<Integer> renderer1 = createRenderer(
+                    new OrderListenerRenderer<Integer>(1, currentIndex, orderErrors));
+            TestRenderer<Integer> renderer2 = createRenderer(
+                    new OrderListenerRenderer<Integer>(2, currentIndex, orderErrors));
+            TestRenderer<Integer> renderer3 = createRenderer(
+                    new OrderListenerRenderer<Integer>(3, currentIndex, orderErrors));
+
+            RenderingState state1 = asyncRenderer.render(
+                renderingKey,
+                Cancellation.UNCANCELABLE_TOKEN,
+                null,
+                renderer1);
+            RenderingState state2 = asyncRenderer.render(
+                renderingKey,
+                Cancellation.UNCANCELABLE_TOKEN,
+                null,
+                renderer2);
+            RenderingState state3 = asyncRenderer.render(
+                renderingKey,
+                Cancellation.UNCANCELABLE_TOKEN,
+                null,
+                renderer3);
+
+            renderer1.allowAll();
+            renderer2.allowAll();
+            renderer3.allowAll();
+
+            AsyncReport report = renderer3.awaitDone();
+
+            assertTrue("Rendering state is expected to be completed.", state1.isRenderingFinished());
+            assertTrue("Rendering state is expected to be completed.", state2.isRenderingFinished());
+            assertTrue("Rendering state is expected to be completed.", state3.isRenderingFinished());
+
+            assertNotNull(report);
+            assertEquals(true, report.isSuccess());
+
+            assertTrue("Second rendering request must be skipped.", !renderer2.isStartRenderingCalled());
+
+            checkCommonErrors(renderer1);
+            checkCommonErrors(renderer2);
+            checkCommonErrors(renderer3);
+
+            if (!orderErrors.isEmpty()) {
+                fail(orderErrors.toString());
+            }
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    private static final class OrderListenerRenderer<DataType> implements DataRenderer<DataType> {
+        private final int ourIndex;
+        private final AtomicInteger currentIndex;
+        private final Queue<String> errorList;
+
+        public OrderListenerRenderer(
+                int ourIndex,
+                AtomicInteger currentIndex,
+                Queue<String> errorList) {
+
+            this.ourIndex = ourIndex;
+            this.currentIndex = currentIndex;
+            this.errorList = errorList;
+        }
+
+        @Override
+        public void startRendering() {
+            int prevIndex;
+            do {
+                prevIndex = currentIndex.get();
+                if (prevIndex >= ourIndex) {
+                    errorList.add("The renderer was called at the wrong time."
+                            + "Current index = " + prevIndex
+                            + ", Index = " + ourIndex);
+                }
+            } while (!currentIndex.compareAndSet(prevIndex, ourIndex));
+        }
+
+        @Override
+        public boolean render(DataType data) {
+            return true;
+        }
+
+        @Override
+        public void finishRendering(AsyncReport report) {
+        }
+    }
+
+    private static class TestDataLink<DataType> implements AsyncDataLink<DataType> {
+        private final TaskExecutor executor;
+        private final List<DataType> datas;
+
+        public TestDataLink(TaskExecutor executor, List<DataType> datas) {
+            this.executor = executor;
+            this.datas = new ArrayList<>(datas);
+        }
+
+        @Override
+        public AsyncDataController getData(
+                CancellationToken cancelToken,
+                final AsyncDataListener<? super DataType> dataListener) {
+            ExceptionHelper.checkNotNullArgument(cancelToken, "cancelToken");
+            ExceptionHelper.checkNotNullArgument(dataListener, "dataListener");
+
+            final AtomicInteger step = new AtomicInteger(0);
+
+            executor.execute(cancelToken, new CancelableTask() {
+                @Override
+                public void execute(CancellationToken cancelToken) {
+                    for (DataType data: datas) {
+                        cancelToken.checkCanceled();
+
+                        dataListener.onDataArrive(data);
+                        step.incrementAndGet();
+                    }
+                }
+            }, new CleanupTask() {
+                @Override
+                public void cleanup(boolean canceled, Throwable error) {
+                    dataListener.onDoneReceive(AsyncReport.getReport(error, canceled));
+                }
+            });
+
+            return new AsyncDataController() {
+                @Override
+                public void controlData(Object controlArg) {
+                }
+
+                @Override
+                public AsyncDataState getDataState() {
+                    double progress = (double)step.get() / (double)datas.size();
+                    return new SimpleDataState("", progress);
+                }
+            };
+        }
+    }
+}
---
+++ jtrim-gui/src/test/java/org/jtrim/swing/concurrent/async/TestRenderer.java
@@ -0,0 +1,189 @@
+package org.jtrim.swing.concurrent.async;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import org.jtrim.cancel.Cancellation;
+import org.jtrim.cancel.CancellationToken;
+import org.jtrim.concurrent.WaitableSignal;
+import org.jtrim.concurrent.async.AsyncReport;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public final class TestRenderer<DataType> implements DataRenderer<DataType> {
+    private final DataRenderer<DataType> renderer;
+    private final CountDownLatch startRenderingLatch;
+    private final CountDownLatch renderLatch;
+    private final CountDownLatch finishRenderingLatch;
+    private final WaitableSignal doneSignal;
+    private final long timeoutNanos;
+    private final AtomicReference<String> callInProgress;
+    private final AtomicInteger startCallCount;
+    private final AtomicInteger finishCallCount;
+    private final Queue<DataType> receivedDatas;
+    private final Queue<String> miscErrors;
+    private AsyncReport finalReport;
+
+    public TestRenderer(long timeout, TimeUnit unit) {
+        this(null, timeout, unit);
+    }
+
+    public TestRenderer(DataRenderer<DataType> renderer, long timeout, TimeUnit unit) {
+        this.renderer = renderer;
+        this.startRenderingLatch = new CountDownLatch(1);
+        this.renderLatch = new CountDownLatch(1);
+        this.finishRenderingLatch = new CountDownLatch(1);
+        this.doneSignal = new WaitableSignal();
+        this.timeoutNanos = unit.toNanos(timeout);
+        this.startCallCount = new AtomicInteger(0);
+        this.finishCallCount = new AtomicInteger(0);
+        this.callInProgress = new AtomicReference<>(null);
+        this.receivedDatas = new ConcurrentLinkedQueue<>();
+        this.miscErrors = new ConcurrentLinkedQueue<>();
+        this.finalReport = null;
+    }
+
+    public boolean isStartRenderingCalled() {
+        return startCallCount.get() > 0;
+    }
+
+    public int getRenderCallCount() {
+        return receivedDatas.size();
+    }
+
+    public boolean isFinishRenderingCalled() {
+        return finishCallCount.get() > 0;
+    }
+
+    public List<DataType> getReceivedDatas() {
+        return new LinkedList<>(receivedDatas);
+    }
+
+    public List<String> getMiscErrors() {
+        return new LinkedList<>(miscErrors);
+    }
+
+    public void allowStartRendering() {
+        startRenderingLatch.countDown();
+    }
+
+    public void allowRender() {
+        renderLatch.countDown();
+    }
+
+    public void allowFinishRendering() {
+        finishRenderingLatch.countDown();
+    }
+
+    public void allowAll() {
+        allowStartRendering();
+        allowRender();
+        allowFinishRendering();
+    }
+
+    public AsyncReport awaitDone() {
+        return awaitDone(Cancellation.UNCANCELABLE_TOKEN);
+    }
+
+    public AsyncReport awaitDone(CancellationToken cancelToken) {
+        doneSignal.waitSignal(cancelToken, timeoutNanos, TimeUnit.NANOSECONDS);
+        return finalReport;
+    }
+
+    private void awaitLatch(CountDownLatch latch) {
+        try {
+            latch.await(timeoutNanos, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ex) {
+            Thread.interrupted();
+        }
+    }
+
+    private void stateError(String errorStr) {
+        miscErrors.add(errorStr);
+        throw new IllegalStateException(errorStr);
+    }
+
+    private void enterCall(String call) {
+        String currentCall;
+        currentCall = callInProgress.get();
+        if (currentCall != null) {
+            String errorStr = "Attempting to call " + call + " concurrently with " + currentCall;
+            stateError(errorStr);
+        }
+    }
+
+    private void leaveCall() {
+        callInProgress.set(null);
+    }
+
+    @Override
+    public void startRendering() {
+        enterCall("startRendering");
+        try {
+            if (startCallCount.getAndIncrement() > 0) {
+                stateError("startRendering has been called multiple times.");
+            }
+            if (doneSignal.isSignaled()) {
+                stateError("startRendering has been called after finishRendering.");
+            }
+            awaitLatch(startRenderingLatch);
+            if (renderer != null) {
+                renderer.startRendering();
+            }
+        } finally {
+            leaveCall();
+        }
+    }
+
+    @Override
+    public boolean render(DataType data) {
+        enterCall("render");
+        try {
+            if (startCallCount.get() == 0) {
+                stateError("render has been called without calling startRendering.");
+            }
+            if (doneSignal.isSignaled()) {
+                stateError("render has been called after finishRendering.");
+            }
+            awaitLatch(renderLatch);
+            receivedDatas.add(data);
+            if (renderer != null) {
+                return renderer.render(data);
+            }
+            else {
+                return false;
+            }
+        } finally {
+            leaveCall();
+        }
+    }
+
+    @Override
+    public void finishRendering(AsyncReport report) {
+        enterCall("finishRendering");
+        try {
+            if (finishCallCount.getAndIncrement() > 0) {
+                stateError("finishRendering has been called multiple times.");
+            }
+            if (startCallCount.get() == 0) {
+                stateError("finishRendering has been called without calling startRendering.");
+            }
+            awaitLatch(finishRenderingLatch);
+            finalReport = report;
+            if (renderer != null) {
+                renderer.finishRendering(report);
+            }
+        } finally {
+            doneSignal.signal();
+            leaveCall();
+        }
+    }
+
+}
