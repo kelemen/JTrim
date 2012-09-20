9520e64c49754f9593720912599487d6eaed3db6
Attila Kelemen <attila.kelemen85@gmail.com>
2012-09-20 21:41:46 +0200

    Documented Graphics2DComponent

--- jtrim-gui/src/main/java/org/jtrim/swing/component/Graphics2DComponent.java
+++ jtrim-gui/src/main/java/org/jtrim/swing/component/Graphics2DComponent.java
@@ -7,6 +7,16 @@ import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 
 /**
+ * Defines a convenient base class for user drawn <I>Swing</I> components. This
+ * class defines an abstract {@link #paintComponent2D(Graphics2D) paintComponent2D}
+ * in place of the inherited {@link #paintComponent(Graphics) paintComponent}
+ * method. The main difference between these methods is that
+ * {@code paintComponent2D} takes a {@code Graphics2D} object rather than a
+ * {@code Graphics}.
+ * <P>
+ * The thread-safety property of this component is the same as with any other
+ * <I>Swing</I> components. That is, instances of this class can be accessed
+ * only from the AWT Event Dispatch Thread after made displayable.
  *
  * @author Kelemen Attila
  */
@@ -16,12 +26,40 @@ public abstract class Graphics2DComponent extends JComponent {
 
     private BufferedImage fallbackImage;
 
+    /**
+     * Initializes this Graphics2DComponent.
+     */
     public Graphics2DComponent() {
         this.fallbackImage = null;
     }
 
+    /**
+     * Subclasses must override this method to paint this component.
+     * Implementations must honor the {@code opaque} property of this component
+     * but does not need to preserve the context of the passed
+     * {@code Graphics2D} object (unlike with {@code paintComponent}).
+     *
+     * @param g the {@code Graphics2D} to paint to. This argument cannot be
+     *   {@code null}.
+     */
     protected abstract void paintComponent2D(Graphics2D g);
 
+    /**
+     * Invokes {@link #paintComponent2D(Graphics2D) paintComponent2D} with a
+     * {@code Graphics2D} object to draw on.
+     * <P>
+     * In case a {@code Graphics2D} object is passed to this method a copy of
+     * the context (via {@code Graphics2D#.reate()}) will be created and passed
+     * to {@code paintComponent2D}, otherwise a {@code BufferedImage} of the
+     * type {@code BufferedImage.TYPE_INT_ARGB} will be created to draw on and
+     * its {@code Graphics2D} will be passed to {@code paintComponent2D}.
+     * <P>
+     * This method cannot be overridden, override {@code paintComponent2D}
+     * instead.
+     *
+     * @param g the {@code Graphics} to paint to. This argument cannot be
+     *   {@code null}.
+     */
     @Override
     protected final void paintComponent(Graphics g) {
         int currentWidth = getWidth();
