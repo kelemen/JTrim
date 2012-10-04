56d077bb8b7c1b0d73111f266532b9143ee1aa7a
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-04 23:37:29 +0200

    Added package-info for "org.jtrim.swing.component"

---
+++ jtrim-gui/src/main/java/org/jtrim/swing/component/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains classes and interfaces related to swing components. This package
+ * mostly about components rendered on a background thread. See
+ * {@link org.jtrim.swing.component.AsyncRenderingComponent} for the base class
+ * for such components.
+ *
+ * @see org.jtrim.swing.component.AsyncRenderingComponent
+ */
+package org.jtrim.swing.component;
