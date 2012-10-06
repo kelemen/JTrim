3dbeccdf8c7c3572d22ea5a5bca3b01b3287097c
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-06 21:51:38 +0200

    Documented ImageTransformer

--- jtrim-gui/src/main/java/org/jtrim/image/transform/ImageTransformer.java
+++ jtrim-gui/src/main/java/org/jtrim/image/transform/ImageTransformer.java
@@ -1,13 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.image.transform;
 
 import org.jtrim.concurrent.async.DataConverter;
 
 /**
+ * Defines an image transformation from {@link ImageTransformerData} to
+ * {@link TransformedImage}.
+ * <P>
+ * When applying a transformation on the input, the conversion must specify
+ * the coordinate transformation which transforms the coordinates of the source
+ * image to the coordinates of the resulting image. That is, where a pixel of
+ * the original image can be found on the resulting image. The coordinate
+ * transformation must support coordinates laying outside the bounds of the
+ * image.
+ *
+ * <h3>Thread safety</h3>
+ * Implementations of this interface are required to be safe to be used by
+ * multiple threads concurrently.
+ *
+ * <h4>Synchronization transparency</h4>
+ * Implementations of this interface are not required to be
+ * <I>synchronization transparent</I>.
+ *
+ * @see ImageTransformerLink
+ * @see ImageTransfromerQuery
+ * @see org.jtrim.swing.component.AsyncImageDisplay
  *
  * @author Kelemen Attila
  */
