700a13c2b245f36f7411472bf05571f329fd32b4
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-07 21:51:26 +0200

    Remove meaningless comment from ImageConverter, made the class final and added links to where this class is used.

--- jtrim-gui/src/main/java/org/jtrim/image/transform/ImageConverter.java
+++ jtrim-gui/src/main/java/org/jtrim/image/transform/ImageConverter.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jtrim.image.transform;
 
 import org.jtrim.concurrent.async.AsyncFormatHelper;
@@ -9,10 +5,12 @@ import org.jtrim.concurrent.async.DataConverter;
 import org.jtrim.utils.ExceptionHelper;
 
 /**
+ * @see ImageTransformerLink
+ * @see ImageTransfromerQuery
  *
  * @author Kelemen Attila
  */
-class ImageConverter
+final class ImageConverter
 implements
         DataConverter<ImageTransformerData, TransformedImageData> {
 
