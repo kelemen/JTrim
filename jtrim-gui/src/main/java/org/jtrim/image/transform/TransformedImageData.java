07fe55b419bdb5f64214cd5b4cd9476ccbb51c2c
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-13 18:15:53 +0200

    Documented TransformedImageData

--- jtrim-gui/src/main/java/org/jtrim/image/transform/TransformedImageData.java
+++ jtrim-gui/src/main/java/org/jtrim/image/transform/TransformedImageData.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.jtrim.image.transform;
 
 import java.awt.image.BufferedImage;
@@ -10,6 +5,20 @@ import org.jtrim.cache.MemoryHeavyObject;
 import org.jtrim.image.ImageReceiveException;
 
 /**
+ * Defines the output of an {@link ImageTransformerLink}.
+ *
+ * <h3>Thread safety</h3>
+ * Methods of this class can be safely accessed by multiple threads. Although
+ * individual properties are not immutable, they should be treated so. Users of
+ * this class can assume that the properties of {@code ImageTransformerData} are
+ * not modified and can be safely accessed by multiple concurrent threads as
+ * well.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The methods of this class are <I>synchronization transparent</I>.
+ *
+ * @see ImageTransformerLink
+ * @see ImageTransformerQuery
  *
  * @author Kelemen Attila
  */
@@ -17,27 +26,79 @@ public final class TransformedImageData implements MemoryHeavyObject {
     private final TransformedImage transformedImage;
     private final ImageReceiveException exception;
 
+    /**
+     * Creates a new {@code TransformedImageData} with the given properties.
+     *
+     * @param transformedImage the result of the image transformation. This
+     *   argument can be {@code null} if the transformation resulted in
+     *   {@code null}.
+     * @param exception the exception occurred when the image was retrieved.
+     *   This argument can be {@code null} if there were no errors.
+     */
     public TransformedImageData(TransformedImage transformedImage, ImageReceiveException exception) {
         this.transformedImage = transformedImage;
         this.exception = exception;
     }
 
+    /**
+     * Returns the exception occurred when the image was retrieved. This is the
+     * same exception object as the one specified at construction time.
+     *
+     * @return the exception occurred when the image was retrieved. This method
+     *   may return {@code null}.
+     */
     public ImageReceiveException getException() {
         return exception;
     }
 
+    /**
+     * Returns the result of the image transformation. This is the same object
+     * as the one one specified at construction time.
+     *
+     * @return the result of the image transformation. This method may return
+     *   {@code null}.
+     */
     public TransformedImage getTransformedImage() {
         return transformedImage;
     }
 
+    /**
+     * Returns the {@link TransformedImage#getPointTransformer() PointTransformer}
+     * property of the {@link #getTransformedImage() result of the image transformation}
+     * or {@code null} if the {@link #getTransformedImage() TransformedImage}
+     * property is {@code null}.
+     *
+     * @return the {@link TransformedImage#getPointTransformer() PointTransformer}
+     *   property of the {@link #getTransformedImage() result of the image transformation}
+     *   or {@code null} if the {@link #getTransformedImage() TransformedImage}
+     *   property is {@code null}
+     */
     public ImagePointTransformer getPointTransformer() {
         return transformedImage != null ? transformedImage.getPointTransformer() : null;
     }
 
+    /**
+     * Returns the {@link TransformedImage#getImage() resulting image} of the
+     * image transformation or {@code null} if the
+     * {@link #getTransformedImage() TransformedImage} property is {@code null}.
+     *
+     * @return the {@link TransformedImage#getImage() resulting image} of the
+     *   image transformation or {@code null} if the
+     *   {@link #getTransformedImage() TransformedImage} property is {@code null}
+     */
     public BufferedImage getImage() {
         return transformedImage != null ? transformedImage.getImage() : null;
     }
 
+    /**
+     * Returns the approximate memory retention in bytes of the resulting image
+     * or 0 if the {@link #getTransformedImage() TransformedImage} property is
+     * {@code null}.
+     *
+     * @return the approximate memory retention in bytes of the resulting image
+     *   or 0 if the {@link #getTransformedImage() TransformedImage} property is
+     *   {@code null}
+     */
     @Override
     public long getApproxMemorySize() {
         if (transformedImage != null) {
