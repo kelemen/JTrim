d52b008811d1bff45623f63f18afbbc0bb741fdc
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-17 00:43:53 +0200

    Added test methods for CollectionsEx, ElementRefIterable and NaturalComparator.

---
+++ jtrim-core/src/test/java/org/jtrim/collections/ElementRefIterableTest.java
@@ -0,0 +1,59 @@
+package org.jtrim.collections;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import org.jtrim.collections.RefList.ElementRef;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class ElementRefIterableTest {
+
+    public ElementRefIterableTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of iterator method, of class ElementRefIterable.
+     */
+    @Test
+    public void testIterator() {
+        int[] expected = new int[]{10, 11, 13};
+        RefList<Integer> list = new RefLinkedList<>();
+        for (int i = 0; i < expected.length; i++) {
+            list.add(expected[i]);
+        }
+
+        ElementRefIterable<Integer> iterable = new ElementRefIterable<>(list);
+        Iterator<ElementRef<Integer>> itr = iterable.iterator();
+        assertTrue(itr instanceof ElementRefIterator);
+
+        for (int i = 0; i < expected.length; i++) {
+            assertTrue(itr.hasNext());
+            assertEquals(expected[i], itr.next().getElement().intValue());
+        }
+        assertFalse(itr.hasNext());
+    }
+}
