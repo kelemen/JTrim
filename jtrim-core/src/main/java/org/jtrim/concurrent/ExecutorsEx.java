a27700c52f1645af19afa41921efb9fdcdd21feb
Attila Kelemen <attila.kelemen85@gmail.com>
2012-11-17 22:03:51 +0100

    Minor adjustment to ExecutorsEx

--- jtrim-core/src/main/java/org/jtrim/concurrent/ExecutorsEx.java
+++ jtrim-core/src/main/java/org/jtrim/concurrent/ExecutorsEx.java
@@ -442,6 +442,7 @@ public final class ExecutorsEx {
         result = new ScheduledThreadPoolExecutor(maxThreadCount,
                 new NamedThreadFactory(isDaemon, poolName),
                 REJECT_POLICY);
+        result.setMaximumPoolSize(maxThreadCount);
 
         return result;
     }
