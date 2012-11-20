f07e600fcfd40b76a73a082dcff7d50001296435
Attila Kelemen <attila.kelemen85@gmail.com>
2012-11-20 23:52:33 +0100

    Added more tests for OneShotListenerManager and adjusted the already added ones.

--- jtrim-core/src/test/java/org/jtrim/event/OneShotListenerManagerTest.java
+++ jtrim-core/src/test/java/org/jtrim/event/OneShotListenerManagerTest.java
@@ -1,9 +1,10 @@
 package org.jtrim.event;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.CountDownLatch;
 import org.junit.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -30,108 +31,224 @@ public class OneShotListenerManagerTest {
     public void tearDown() {
     }
 
+    private static OneShotListenerManager<ObjectEventListener, Object> create() {
+        return new OneShotListenerManager<>();
+    }
+
+    @Test
+    public void testGetListenerCount() {
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
+
+        ObjectEventListener listener1 = mock(ObjectEventListener.class);
+        ObjectEventListener listener2 = mock(ObjectEventListener.class);
+        ObjectEventListener listener3 = mock(ObjectEventListener.class);
+
+        assertEquals(0, manager.getListenerCount());
+
+        ListenerRef ref = manager.registerListener(listener1);
+        assertEquals(1, manager.getListenerCount());
+
+        manager.registerListener(listener2);
+        assertEquals(2, manager.getListenerCount());
+
+        manager.registerListener(listener3);
+        assertEquals(3, manager.getListenerCount());
+
+        ref.unregister();
+        assertEquals(2, manager.getListenerCount());
+
+        verifyZeroInteractions(listener1, listener2, listener3);
+
+        Object eventArg = new Object();
+        manager.onEvent(ObjectDispatcher.INSTANCE, eventArg);
+
+        verify(listener2).onEvent(same(eventArg));
+        verify(listener3).onEvent(same(eventArg));
+        verifyNoMoreInteractions(listener1, listener2, listener3);
+
+        assertEquals(0, manager.getListenerCount());
+    }
+
     @Test
     public void testOnEventBeforeRegister() {
-        Integer testArg = 10;
+        Object testArg = new Object();
 
-        OneShotListenerManager<IntegerEventListener, Integer> manager;
-        manager = new OneShotListenerManager<>();
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
 
-        final AtomicReference<Integer> received = new AtomicReference<>(null);
-
-        manager.onEvent(IntegerDispatcher.INSTANCE, testArg);
-        ListenerRef ref = manager.registerListener(new IntegerEventListener() {
-            @Override
-            public void onEvent(Integer arg) {
-                received.set(arg);
-            }
-        });
+        manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
+        ObjectEventListener listener = mock(ObjectEventListener.class);
+        ListenerRef ref = manager.registerListener(listener);
 
         assertTrue("Listener was unregistered", ref.isRegistered());
-        assertNull("Listener has been notified", received.get());
+        verifyZeroInteractions(listener);
     }
 
     @Test
     public void testOnEventAfterRegister() {
-        Integer testArg = 10;
+        Object testArg = new Object();
 
-        OneShotListenerManager<IntegerEventListener, Integer> manager;
-        manager = new OneShotListenerManager<>();
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
 
-        final AtomicReference<Integer> received = new AtomicReference<>(null);
-
-        ListenerRef ref = manager.registerListener(new IntegerEventListener() {
-            @Override
-            public void onEvent(Integer arg) {
-                received.set(arg);
-            }
-        });
+        ObjectEventListener listener = mock(ObjectEventListener.class);
+        ListenerRef ref = manager.registerListener(listener);
         assertTrue("Listener was unregistered", ref.isRegistered());
-        assertNull("Listener has been notified", received.get());
+        verifyZeroInteractions(listener);
 
-        manager.onEvent(IntegerDispatcher.INSTANCE, testArg);
+        manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
 
         assertFalse("Listener is registered", ref.isRegistered());
-        assertEquals("Listener was not notified", testArg, received.get());
+        verify(listener).onEvent(same(testArg));
+        verifyNoMoreInteractions(listener);
     }
 
     @Test
     public void testOnEventBeforeRegisterOrNotify() {
-        Integer testArg = 10;
+        Object testArg = new Object();
 
-        OneShotListenerManager<IntegerEventListener, Integer> manager;
-        manager = new OneShotListenerManager<>();
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
 
-        final AtomicReference<Integer> received = new AtomicReference<>(null);
+        manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
 
-        manager.onEvent(IntegerDispatcher.INSTANCE, testArg);
-        ListenerRef ref = manager.registerOrNotifyListener(new IntegerEventListener() {
-            @Override
-            public void onEvent(Integer arg) {
-                received.set(arg);
-            }
-        });
+        ObjectEventListener listener = mock(ObjectEventListener.class);
+        ListenerRef ref = manager.registerOrNotifyListener(listener);
 
         assertFalse("Listener is registered", ref.isRegistered());
-        assertEquals("Listener was not notified", testArg, received.get());
+        verify(listener).onEvent(same(testArg));
+        verifyNoMoreInteractions(listener);
     }
 
     @Test
     public void testOnEventAfterRegisterOrNotify() {
-        Integer testArg = 10;
+        Object testArg = new Object();
 
-        OneShotListenerManager<IntegerEventListener, Integer> manager;
-        manager = new OneShotListenerManager<>();
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
 
-        final AtomicReference<Integer> received = new AtomicReference<>(null);
-
-        ListenerRef ref = manager.registerOrNotifyListener(new IntegerEventListener() {
-            @Override
-            public void onEvent(Integer arg) {
-                received.set(arg);
-            }
-        });
+        ObjectEventListener listener = mock(ObjectEventListener.class);
+        ListenerRef ref = manager.registerOrNotifyListener(listener);
         assertTrue("Listener was unregistered", ref.isRegistered());
-        assertNull("Listener has been notified", received.get());
+        verifyZeroInteractions(listener);
 
-        manager.onEvent(IntegerDispatcher.INSTANCE, testArg);
+        manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
 
         assertFalse("Listener is registered", ref.isRegistered());
-        assertEquals("Listener was not notified", testArg, received.get());
+        verify(listener).onEvent(same(testArg));
+        verifyNoMoreInteractions(listener);
     }
 
-    private interface IntegerEventListener {
-        public void onEvent(Integer arg);
+    public void doTestConcurrentRegisterWithEvent() throws InterruptedException {
+        final Object testArg = new Object();
+        final OneShotListenerManager<ObjectEventListener, Object> manager = create();
+
+        int listenerCount = 2 * Runtime.getRuntime().availableProcessors();
+
+        final ObjectEventListener[] listeners = new ObjectEventListener[listenerCount];
+        for (int i = 0; i < listeners.length; i++) {
+            listeners[i] = mock(ObjectEventListener.class);
+        }
+
+        final CountDownLatch startLatch = new CountDownLatch(listenerCount + 1);
+        Thread[] registerThreads = new Thread[listenerCount];
+        for (int i = 0; i < registerThreads.length; i++) {
+            final int threadIndex = i;
+            registerThreads[i] = new Thread() {
+                @Override
+                public void run() {
+                    startLatch.countDown();
+                    try {
+                        startLatch.await();
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException();
+                    }
+                    manager.registerListener(listeners[threadIndex]);
+                    manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
+                }
+            };
+        }
+        Thread notifyThread = new Thread() {
+            @Override
+            public void run() {
+                startLatch.countDown();
+                try {
+                    startLatch.await();
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException();
+                }
+                manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
+            }
+        };
+
+        for (int i = 0; i < registerThreads.length; i++) {
+            registerThreads[i].start();
+        }
+        notifyThread.start();
+
+        for (int i = 0; i < registerThreads.length; i++) {
+            registerThreads[i].join();
+        }
+        notifyThread.join();
+
+        for (int i = 0; i < listeners.length; i++) {
+            ObjectEventListener listener = listeners[i];
+            verify(listener).onEvent(same(testArg));
+            verifyNoMoreInteractions(listener);
+        }
     }
 
-    private enum IntegerDispatcher
+    @Test(timeout = 20000)
+    public void testConcurrentRegisterWithEvent() throws InterruptedException {
+        for (int i = 0; i < 200; i++) {
+            doTestConcurrentRegisterWithEvent();
+        }
+    }
+
+    @Test
+    public void testFailedListener() {
+        Object testArg = new Object();
+
+        OneShotListenerManager<ObjectEventListener, Object> manager = create();
+
+        ObjectEventListener listener1 = mock(ObjectEventListener.class);
+        ObjectEventListener listener2 = mock(ObjectEventListener.class);
+        ObjectEventListener listener3 = mock(ObjectEventListener.class);
+
+        RuntimeException exception1 = new RuntimeException();
+        RuntimeException exception2 = new RuntimeException();
+
+        doThrow(exception1).when(listener1).onEvent(any());
+        doThrow(exception2).when(listener2).onEvent(any());
+
+        manager.registerListener(listener1);
+        manager.registerListener(listener2);
+        manager.registerListener(listener3);
+
+        try {
+            manager.onEvent(ObjectDispatcher.INSTANCE, testArg);
+            fail("Exception expected.");
+        } catch (RuntimeException ex) {
+            assertSame(exception1, ex);
+            Throwable[] suppressed = ex.getSuppressed();
+            assertEquals(1, suppressed.length);
+            assertSame(exception2, suppressed[0]);
+        }
+
+        verify(listener1).onEvent(same(testArg));
+        verify(listener2).onEvent(same(testArg));
+        verify(listener3).onEvent(same(testArg));
+        verifyNoMoreInteractions(listener1, listener2, listener3);
+    }
+
+    private interface ObjectEventListener {
+        public void onEvent(Object arg);
+    }
+
+    private enum ObjectDispatcher
     implements
-            EventDispatcher<IntegerEventListener, Integer> {
+            EventDispatcher<ObjectEventListener, Object> {
 
         INSTANCE;
 
         @Override
-        public void onEvent(IntegerEventListener eventListener, Integer arg) {
+        public void onEvent(ObjectEventListener eventListener, Object arg) {
             eventListener.onEvent(arg);
         }
     }
