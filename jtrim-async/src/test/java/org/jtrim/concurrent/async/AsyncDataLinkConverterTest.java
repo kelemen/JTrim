653b7ba4a54c00c3dbb6609663e51843fa36453c
Attila Kelemen <attila.kelemen85@gmail.com>
2012-12-18 22:36:05 +0100

    Added tests for AsyncDataLinkConverter

---
+++ jtrim-async/src/test/java/org/jtrim/concurrent/async/AsyncDataLinkConverterTest.java
@@ -0,0 +1,103 @@
+package org.jtrim.concurrent.async;
+
+import org.jtrim.cancel.Cancellation;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class AsyncDataLinkConverterTest {
+
+    public AsyncDataLinkConverterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private static <OldDataType, NewDataType> AsyncDataLinkConverter<OldDataType, NewDataType> create(
+            AsyncDataLink<? extends OldDataType> wrappedDataLink,
+            DataConverter<? super OldDataType, ? extends NewDataType> converter) {
+        return new AsyncDataLinkConverter<>(wrappedDataLink, converter);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <DataType> AsyncDataLink<DataType> mockLink() {
+        return mock(AsyncDataLink.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <OldDataType, NewDataType> DataConverter<OldDataType, NewDataType> mockConverter() {
+        return mock(DataConverter.class);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testSimpleConversion() {
+        Object input = new Object();
+        Object output = new Object();
+
+        DataConverter<Object, Object> converter = mockConverter();
+
+        AsyncDataController wrappedController = mock(AsyncDataController.class);
+        AsyncDataState wrappedState = mock(AsyncDataState.class);
+        ManualDataLink<Object> wrappedLink = new ManualDataLink<>(wrappedState);
+
+        stub(wrappedController.getDataState()).toReturn(wrappedState);
+        stub(converter.convertData(any())).toReturn(output);
+
+        @SuppressWarnings("unchecked")
+        AsyncDataListener<Object> listener = mock(AsyncDataListener.class);
+
+        AsyncDataController controller = create(wrappedLink, converter)
+                .getData(Cancellation.UNCANCELABLE_TOKEN, listener);
+
+        verifyZeroInteractions(wrappedController);
+
+        Object[] controlArgs = new Object[]{new Object(), new Object()};
+        controller.controlData(controlArgs[0]);
+        controller.controlData(controlArgs[1]);
+        assertSame(wrappedState, controller.getDataState());
+
+        assertArrayEquals(controlArgs, wrappedLink.getForwardedDatas().toArray());
+
+        AsyncReport report = AsyncReport.getReport(new Exception(), false);
+
+        wrappedLink.onDataArrive(input);
+        verify(listener).onDataArrive(same(output));
+        verifyNoMoreInteractions(listener);
+
+        wrappedLink.onDoneReceive(report);
+        verify(listener).onDoneReceive(same(report));
+        verifyNoMoreInteractions(listener);
+    }
+
+    /**
+     * Test of toString method, of class AsyncDataLinkConverter.
+     */
+    @Test
+    public void testToString() {
+        assertNotNull(create(mockLink(), mockConverter()).toString());
+    }
+
+}
