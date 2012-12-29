8bed877a59cb4649b2755c353c28ed58ad0c773a
Attila Kelemen <attila.kelemen85@gmail.com>
2012-12-29 23:46:57 +0100

    Added tests for PeriodicStateReporterLink

--- jtrim-async/src/test/java/org/jtrim/concurrent/async/AsyncMocks.java
+++ jtrim-async/src/test/java/org/jtrim/concurrent/async/AsyncMocks.java
@@ -1,5 +1,7 @@
 package org.jtrim.concurrent.async;
 
+import org.jtrim.cancel.CancellationToken;
+
 import static org.mockito.Mockito.*;
 
 /**
@@ -20,6 +22,12 @@ public final class AsyncMocks {
     @SuppressWarnings("unchecked")
     public static <DataType> AsyncDataListener<DataType> mockListener() {
         return mock(AsyncDataListener.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void stubController(AsyncDataLink<?> mockLink, AsyncDataController controller) {
+        stub(mockLink.getData(any(CancellationToken.class), any(AsyncDataListener.class)))
+                .toReturn(controller);
     }
 
     private AsyncMocks() {
