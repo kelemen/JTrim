a28cad65d50c90f32b3d8ed54027a732e9a2cb4b
Attila Kelemen <attila.kelemen85@gmail.com>
2013-01-18 00:12:41 +0100

    Simplified the implementation of AbstractAccessToken.

--- jtrim-gui/src/main/java/org/jtrim/access/AbstractAccessToken.java
+++ jtrim-gui/src/main/java/org/jtrim/access/AbstractAccessToken.java
@@ -1,10 +1,8 @@
 package org.jtrim.access;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import org.jtrim.cancel.CancellationToken;
 import org.jtrim.event.*;
-import org.jtrim.utils.ExceptionHelper;
 
 /**
  * A convenient base class for {@link AccessToken} implementations.
@@ -32,13 +30,13 @@ public abstract class AbstractAccessToken<IDType>
 implements
         AccessToken<IDType> {
 
-    private final ListenerManager<Runnable, Void> listeners;
+    private final OneShotListenerManager<Runnable, Void> listeners;
 
     /**
      * Initializes the {@code AbstractAccessToken}.
      */
     public AbstractAccessToken() {
-        this.listeners = new CopyOnTriggerListenerManager<>();
+        this.listeners = new OneShotListenerManager<>();
     }
 
     /**
@@ -66,25 +64,7 @@ implements
      */
     @Override
     public ListenerRef addReleaseListener(Runnable listener) {
-        ExceptionHelper.checkNotNullArgument(listener, "listener");
-        // A quick check for the already terminate case.
-        if (isReleased()) {
-            listener.run();
-            return UnregisteredListenerRef.INSTANCE;
-        }
-
-        AutoUnregisterListener autoListener = new AutoUnregisterListener(listener);
-        ListenerRef result = autoListener.registerWith(listeners);
-        // If the access token was released before the registration and also
-        // notifyReleaseListeners() was called, there is a chance that this
-        // currently added listener may not have been notified. To avoid this
-        // we have to double check isReleased() and notify the listener.
-        // Note that AutoUnregisterListener.run() is idempotent, so there is no
-        // problem if it actually got called.
-        if (isReleased()) {
-            autoListener.run();
-        }
-        return result;
+        return listeners.registerOrNotifyListener(listener);
     }
 
     /**
@@ -107,40 +87,6 @@ implements
         @Override
         public void onEvent(Runnable eventListener, Void arg) {
             eventListener.run();
-        }
-    }
-
-    // Note that apart from automatic unregistering, this class
-    // takes care that the listener may not be run multiple times.
-    private static class AutoUnregisterListener implements Runnable {
-        private final AtomicReference<Runnable> listener;
-        private volatile ListenerRef listenerRef;
-
-        public AutoUnregisterListener(Runnable listener) {
-            this.listener = new AtomicReference<>(listener);
-            this.listenerRef = null;
-        }
-
-        public ListenerRef registerWith(ListenerManager<Runnable, ?> manager) {
-            ListenerRef currentRef = manager.registerListener(this);
-            this.listenerRef = currentRef;
-            if (listener.get() == null) {
-                this.listenerRef = null;
-                currentRef.unregister();
-            }
-            return currentRef;
-        }
-
-        @Override
-        public void run() {
-            Runnable currentListener = listener.getAndSet(null);
-            ListenerRef currentRef = listenerRef;
-            if (currentRef != null) {
-                currentRef.unregister();
-            }
-            if (currentListener != null) {
-                currentListener.run();
-            }
         }
     }
 }
