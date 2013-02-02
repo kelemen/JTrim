425013d5ca2cf4e97138b934a26bd9c819acfe21
Attila Kelemen <attila.kelemen85@gmail.com>
2013-02-03 00:26:34 +0100

    Added tests for ScheduledAccessToken.

--- jtrim-gui/src/test/java/org/jtrim/access/ScheduledAccessTokenTest.java
+++ jtrim-gui/src/test/java/org/jtrim/access/ScheduledAccessTokenTest.java
@@ -2,10 +2,16 @@ package org.jtrim.access;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import org.jtrim.cancel.Cancellation;
+import org.jtrim.cancel.CancellationSource;
 import org.jtrim.cancel.CancellationToken;
+import org.jtrim.cancel.OperationCanceledException;
 import org.jtrim.concurrent.*;
 import org.junit.*;
+import org.mockito.ArgumentMatcher;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -142,6 +148,220 @@ public class ScheduledAccessTokenTest {
         verify(task).execute(any(CancellationToken.class));
 
         verify(subToken).createExecutor(any(TaskExecutor.class));
+    }
+
+    @Test
+    public void testCancelBeforeAllowedToExecute() {
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+        ManualTaskExecutor manualExecutor = new ManualTaskExecutor();
+        TaskExecutor executor = token.createExecutor(manualExecutor);
+
+        CancellationSource cancelSource = Cancellation.createCancellationSource();
+        CancelableTask task = mock(CancelableTask.class);
+        executor.execute(cancelSource.getToken(), task, null);
+
+        verifyZeroInteractions(task);
+        cancelSource.getController().cancel();
+        blockingToken.release();
+        manualExecutor.executeCurrentlySubmitted();
+        verifyZeroInteractions(task);
+    }
+
+    @Test
+    public void testCancelBeforeAllowedToExecuteWithCleanup() throws Exception {
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+        ManualTaskExecutor manualExecutor = new ManualTaskExecutor();
+        TaskExecutor executor = token.createExecutor(manualExecutor);
+
+        CancellationSource cancelSource = Cancellation.createCancellationSource();
+        CancelableTask task = mock(CancelableTask.class);
+        CleanupTask cleanup = mock(CleanupTask.class);
+        executor.execute(cancelSource.getToken(), task, cleanup);
+
+        verifyZeroInteractions(task, cleanup);
+        cancelSource.getController().cancel();
+        blockingToken.release();
+        manualExecutor.executeCurrentlySubmitted();
+        verifyZeroInteractions(task);
+        verify(cleanup).cleanup(eq(true), argThat(canceledOrNull()));
+    }
+
+    @Test
+    public void testReleaseBeforeAllowedToExecute() throws Exception {
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+        ManualTaskExecutor manualExecutor = new ManualTaskExecutor();
+        TaskExecutor executor = token.createExecutor(manualExecutor);
+
+        token.release();
+
+        CancelableTask task = mock(CancelableTask.class);
+        executor.execute(Cancellation.UNCANCELABLE_TOKEN, task, null);
+
+        blockingToken.release();
+        manualExecutor.executeCurrentlySubmitted();
+        verifyZeroInteractions(task);
+    }
+
+    @Test
+    public void testReleaseBeforeAllowedToExecuteWithCleanup() throws Exception {
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+        ManualTaskExecutor manualExecutor = new ManualTaskExecutor();
+        TaskExecutor executor = token.createExecutor(manualExecutor);
+
+        token.release();
+
+        CancelableTask task = mock(CancelableTask.class);
+        CleanupTask cleanup = mock(CleanupTask.class);
+        executor.execute(Cancellation.UNCANCELABLE_TOKEN, task, cleanup);
+
+        blockingToken.release();
+        manualExecutor.executeCurrentlySubmitted();
+        verifyZeroInteractions(task);
+        verify(cleanup).cleanup(eq(true), argThat(canceledOrNull()));
+    }
+
+    @Test
+    public void testErrorInExecutorDoesNotCorruptUs() throws Exception {
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+
+        TaskExecutor buggyExecutor = mock(TaskExecutor.class);
+        doThrow(RuntimeException.class)
+                .when(buggyExecutor)
+                .execute(any(CancellationToken.class), any(CancelableTask.class), any(CleanupTask.class));
+
+        TaskExecutor executorWithBuggy = token.createExecutor(buggyExecutor);
+
+        executorWithBuggy.execute(Cancellation.UNCANCELABLE_TOKEN, Tasks.noOpCancelableTask(), null);
+        executorWithBuggy.execute(Cancellation.UNCANCELABLE_TOKEN, Tasks.noOpCancelableTask(), null);
+
+        try {
+            blockingToken.release();
+        } catch (RuntimeException ex) {
+            // Ignore exceptions from buggyExecutor
+        }
+
+        TaskExecutor newExecutor = token.createExecutor(SyncTaskExecutor.getSimpleExecutor());
+        CancelableTask task = mock(CancelableTask.class);
+        CleanupTask cleanup = mock(CleanupTask.class);
+        newExecutor.execute(Cancellation.UNCANCELABLE_TOKEN, task, cleanup);
+
+        verify(task).execute(any(CancellationToken.class));
+        verify(cleanup).cleanup(false, null);
+    }
+
+    private void testConcurrentStartAndSubmit(int numberOfTasks) throws Exception {
+        assert numberOfTasks > 0;
+
+        AccessToken<String> subToken = AccessTokens.createToken("INDEPENDENT");
+        final AccessToken<String> blockingToken = AccessTokens.createToken("BLOCKING-TOKEN");
+
+        ScheduledAccessToken<String> token = createBlockedToken(subToken, blockingToken);
+
+        List<Runnable> tasks = new LinkedList<>();
+        final TaskExecutor executor = token.createExecutor(SyncTaskExecutor.getSimpleExecutor());
+
+        CancelableTask[] submittedTasks = new CancelableTask[numberOfTasks];
+        for (int i = 0; i < numberOfTasks; i++) {
+            final CancelableTask submittedTask = mock(CancelableTask.class);
+            submittedTasks[i] = submittedTask;
+            Runnable submitTask = new Runnable() {
+                @Override
+                public void run() {
+                    executor.execute(Cancellation.UNCANCELABLE_TOKEN, submittedTask, null);
+                }
+            };
+            tasks.add(submitTask);
+        }
+
+        tasks.add(new Runnable() {
+            @Override
+            public void run() {
+                blockingToken.release();
+            }
+        });
+
+        runConcurrently(tasks.toArray(new Runnable[tasks.size()]));
+
+        for (int i = 0; i < numberOfTasks; i++) {
+            verify(submittedTasks[i]).execute(any(CancellationToken.class));
+        }
+    }
+
+    @Test(timeout = 20000)
+    public void testConcurrentStartAndSubmit() throws Exception {
+        int numberOfTasks = 2 * Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < 100; i++) {
+            testConcurrentStartAndSubmit(numberOfTasks);
+        }
+    }
+
+    private static void runConcurrently(Runnable[] tasks) {
+        final CountDownLatch latch = new CountDownLatch(tasks.length);
+        Thread[] threads = new Thread[tasks.length];
+        for (int i = 0; i < threads.length; i++) {
+            final Runnable task = tasks[i];
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    latch.countDown();
+
+                    try {
+                        latch.await();
+                    } catch (InterruptedException ex) {
+                        Thread.currentThread().interrupt();
+                    }
+
+                    task.run();
+                }
+            });
+        }
+
+        try {
+            for (int i = 0; i < threads.length; i++) {
+                threads[i].start();
+            }
+        } finally {
+            boolean interrupted = false;
+            for (int i = 0; i < threads.length; i++) {
+                try {
+                    threads[i].join();
+                } catch (InterruptedException ex) {
+                    interrupted = true;
+                }
+            }
+
+            if (interrupted) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Unexpected interrupt.");
+            }
+        }
+    }
+
+    private static ArgumentMatcher<Throwable> canceledOrNull() {
+        return new ArgumentMatcher<Throwable>() {
+            @Override
+            public boolean matches(Object argument) {
+                if (argument instanceof OperationCanceledException) {
+                    return true;
+                }
+                return argument == null;
+            }
+        };
     }
 
     @Test
