a4fd1d9b4a1735a9e6202175206be5f2650ec254
Attila Kelemen <attila.kelemen85@gmail.com>
2013-02-03 19:58:07 +0100

    Added tests for AccessManagers.

---
+++ jtrim-gui/src/test/java/org/jtrim/access/AccessManagersTest.java
@@ -0,0 +1,146 @@
+package org.jtrim.access;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class AccessManagersTest {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <IDType, RightType> AccessManager<IDType, RightType> mockManager() {
+        return mock(AccessManager.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static ArgumentCaptor<AccessRequest<?, ?>> argRequestCapture() {
+        return (ArgumentCaptor<AccessRequest<?, ?>>)(ArgumentCaptor<?>)ArgumentCaptor.forClass(AccessRequest.class);
+    }
+
+    /**
+     * Test of tryGetReadAccess method, of class AccessManagers.
+     */
+    @Test
+    public void testTryGetReadAccess() {
+        AccessManager<Object, Object> manager = mockManager();
+
+        AccessResult<Object> expectedResult = new AccessResult<>(AccessTokens.createToken(new Object()));
+        stub(manager.tryGetAccess((AccessRequest<?, ?>)any(AccessRequest.class)))
+                .toReturn(expectedResult);
+
+        Object requestID = new Object();
+        Collection<Object> rights = Arrays.asList(new Object(), new Object());
+        AccessResult<Object> result = AccessManagers.tryGetReadAccess(manager, requestID, rights);
+
+        assertSame(expectedResult, result);
+
+        ArgumentCaptor<AccessRequest<?, ?>> argRequest = argRequestCapture();
+        verify(manager).tryGetAccess(argRequest.capture());
+
+        AccessRequest<?, ?> request = argRequest.getValue();
+        assertEquals(new HashSet<>(rights), new HashSet<>(request.getReadRights()));
+        assertTrue(request.getWriteRights().isEmpty());
+    }
+
+    /**
+     * Test of tryGetWriteAccess method, of class AccessManagers.
+     */
+    @Test
+    public void testTryGetWriteAccess() {
+        AccessManager<Object, Object> manager = mockManager();
+
+        AccessResult<Object> expectedResult = new AccessResult<>(AccessTokens.createToken(new Object()));
+        stub(manager.tryGetAccess((AccessRequest<?, ?>)any(AccessRequest.class)))
+                .toReturn(expectedResult);
+
+        Object requestID = new Object();
+        Collection<Object> rights = Arrays.asList(new Object(), new Object());
+        AccessResult<Object> result = AccessManagers.tryGetWriteAccess(manager, requestID, rights);
+
+        assertSame(expectedResult, result);
+
+        ArgumentCaptor<AccessRequest<?, ?>> argRequest = argRequestCapture();
+        verify(manager).tryGetAccess(argRequest.capture());
+
+        AccessRequest<?, ?> request = argRequest.getValue();
+        assertEquals(new HashSet<>(rights), new HashSet<>(request.getWriteRights()));
+        assertTrue(request.getReadRights().isEmpty());
+    }
+
+    /**
+     * Test of getScheduledReadAccess method, of class AccessManagers.
+     */
+    @Test
+    public void testGetScheduledReadAccess() {
+        AccessManager<Object, Object> manager = mockManager();
+
+        AccessResult<Object> expectedResult = new AccessResult<>(AccessTokens.createToken(new Object()));
+        stub(manager.getScheduledAccess((AccessRequest<?, ?>)any(AccessRequest.class)))
+                .toReturn(expectedResult);
+
+        Object requestID = new Object();
+        Collection<Object> rights = Arrays.asList(new Object(), new Object());
+        AccessResult<Object> result = AccessManagers.getScheduledReadAccess(manager, requestID, rights);
+
+        assertSame(expectedResult, result);
+
+        ArgumentCaptor<AccessRequest<?, ?>> argRequest = argRequestCapture();
+        verify(manager).getScheduledAccess(argRequest.capture());
+
+        AccessRequest<?, ?> request = argRequest.getValue();
+        assertEquals(new HashSet<>(rights), new HashSet<>(request.getReadRights()));
+        assertTrue(request.getWriteRights().isEmpty());
+    }
+
+    /**
+     * Test of getScheduledWriteAccess method, of class AccessManagers.
+     */
+    @Test
+    public void testGetScheduledWriteAccess() {
+        AccessManager<Object, Object> manager = mockManager();
+
+        AccessResult<Object> expectedResult = new AccessResult<>(AccessTokens.createToken(new Object()));
+        stub(manager.getScheduledAccess((AccessRequest<?, ?>)any(AccessRequest.class)))
+                .toReturn(expectedResult);
+
+        Object requestID = new Object();
+        Collection<Object> rights = Arrays.asList(new Object(), new Object());
+        AccessResult<Object> result = AccessManagers.getScheduledWriteAccess(manager, requestID, rights);
+
+        assertSame(expectedResult, result);
+
+        ArgumentCaptor<AccessRequest<?, ?>> argRequest = argRequestCapture();
+        verify(manager).getScheduledAccess(argRequest.capture());
+
+        AccessRequest<?, ?> request = argRequest.getValue();
+        assertEquals(new HashSet<>(rights), new HashSet<>(request.getWriteRights()));
+        assertTrue(request.getReadRights().isEmpty());
+    }
+}
