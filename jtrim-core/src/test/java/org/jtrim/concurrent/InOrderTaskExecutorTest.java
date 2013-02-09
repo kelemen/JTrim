269090e892a7b5a8237c441c4147805afb794a68
Attila Kelemen <attila.kelemen85@gmail.com>
2013-02-09 16:24:29 +0100

    Fixed a problem in InOrderTaskExecutorTest.

--- jtrim-core/src/test/java/org/jtrim/concurrent/InOrderTaskExecutorTest.java
+++ jtrim-core/src/test/java/org/jtrim/concurrent/InOrderTaskExecutorTest.java
@@ -56,6 +56,7 @@ public class InOrderTaskExecutorTest {
             if (prev != null && task <= prev) {
                 fail("Invalid task order: " + list);
             }
+            prev = task;
         }
     }
 
