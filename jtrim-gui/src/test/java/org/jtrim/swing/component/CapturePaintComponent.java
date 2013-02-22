dce9c00c83ef34be7608cdf0e53788cd4c78a862
Attila Kelemen <attila.kelemen85@gmail.com>
2013-02-22 23:17:31 +0100

    Added CapturePaintComponent (forget to commit in previous commit)

---
+++ jtrim-gui/src/test/java/org/jtrim/swing/component/CapturePaintComponent.java
@@ -0,0 +1,106 @@
+package org.jtrim.swing.component;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+@SuppressWarnings(value = "serial")
+public final class CapturePaintComponent extends JComponent {
+    private BufferedImage image;
+    private Component child;
+
+    public CapturePaintComponent() {
+        super();
+        image = null;
+        super.setLayout(new GridLayout(1, 1, 0, 0));
+    }
+
+    public BufferedImage getChildContent() {
+        return image;
+    }
+
+    @Override
+    public void setLayout(LayoutManager mgr) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Component add(Component comp) {
+        throw new UnsupportedOperationException("Use setChild instead.");
+    }
+
+    @Override
+    public Component add(String name, Component comp) {
+        throw new UnsupportedOperationException("Use setChild instead.");
+    }
+
+    @Override
+    public Component add(Component comp, int index) {
+        throw new UnsupportedOperationException("Use setChild instead.");
+    }
+
+    @Override
+    public void add(Component comp, Object constraints) {
+        throw new UnsupportedOperationException("Use setChild instead.");
+    }
+
+    @Override
+    public void add(Component comp, Object constraints, int index) {
+        throw new UnsupportedOperationException("Use setChild instead.");
+    }
+
+    public void setChild(Component child) {
+        assert child != null;
+        this.child = child;
+        removeAll();
+        super.add(child);
+    }
+
+    @Override
+    public void update(Graphics g) {
+        paint(g);
+    }
+
+    private void paintDefault(Graphics g) {
+        Color prevColor = g.getColor();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(prevColor);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (child == null) {
+            paintDefault(g);
+            return;
+        }
+        int childWidth = child.getWidth();
+        int childHeight = child.getHeight();
+        if (childWidth <= 0 || childHeight <= 0) {
+            paintDefault(g);
+            return;
+        }
+        if (image == null || image.getWidth() != childWidth || image.getHeight() != childHeight) {
+            image = new BufferedImage(childWidth, childHeight, BufferedImage.TYPE_INT_ARGB);
+        }
+        Graphics2D g2d = image.createGraphics();
+        try {
+            g2d.setColor(Color.BLACK);
+            g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
+            child.paint(g2d);
+        } finally {
+            g2d.dispose();
+        }
+        g.drawImage(image, 0, 0, null);
+    }
+
+}
