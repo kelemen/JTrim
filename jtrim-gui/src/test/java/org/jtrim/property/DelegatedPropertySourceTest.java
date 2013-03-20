f943b34f2f8314de4634bf9b7db382cc68da2c9a
Attila Kelemen <attila.kelemen85@gmail.com>
2013-03-20 22:12:18 +0100

    Added tests for DelegatedPropertySource.

---
+++ jtrim-gui/src/test/java/org/jtrim/property/DelegatedPropertySourceTest.java
@@ -0,0 +1,71 @@
+package org.jtrim.property;
+
+import org.jtrim.event.ListenerRef;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class DelegatedPropertySourceTest {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @SuppressWarnings("unchecked")
+    private static PropertySource<Object> mockProperty() {
+        return mock(PropertySource.class);
+    }
+
+    /**
+     * Test of getValue method, of class DelegatedPropertySource.
+     */
+    @Test
+    public void testGetValue() {
+        PropertySource<Object> wrapped = mockProperty();
+        DelegatedPropertySource<Object> delegated = new DelegatedPropertySource<>(wrapped);
+
+        Object value = new Object();
+        stub(wrapped.getValue()).toReturn(value);
+
+        assertSame(value, delegated.getValue());
+        verify(wrapped).getValue();
+        verifyNoMoreInteractions(wrapped);
+    }
+
+    /**
+     * Test of addChangeListener method, of class DelegatedPropertySource.
+     */
+    @Test
+    public void testAddChangeListener() {
+        PropertySource<Object> wrapped = mockProperty();
+        DelegatedPropertySource<Object> delegated = new DelegatedPropertySource<>(wrapped);
+
+        ListenerRef listenerRef = mock(ListenerRef.class);
+        stub(wrapped.addChangeListener(any(Runnable.class))).toReturn(listenerRef);
+
+        Runnable listener = mock(Runnable.class);
+        assertSame(listenerRef, delegated.addChangeListener(listener));
+        verify(wrapped).addChangeListener(same(listener));
+        verifyNoMoreInteractions(wrapped);
+    }
+}
