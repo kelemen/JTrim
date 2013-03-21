61195984f99389b07d339c3b82217937a757125b
Attila Kelemen <attila.kelemen85@gmail.com>
2013-03-21 22:54:00 +0100

    Added tests for NoOpVerifier.

---
+++ jtrim-gui/src/test/java/org/jtrim/property/NoOpVerifierTest.java
@@ -0,0 +1,42 @@
+package org.jtrim.property;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class NoOpVerifierTest {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testNotNull() {
+        Object value = new Object();
+        assertSame(value, NoOpVerifier.getInstance().storeValue(value));
+    }
+
+    @Test
+    public void testNull() {
+        assertNull(NoOpVerifier.getInstance().storeValue(null));
+    }
+}
