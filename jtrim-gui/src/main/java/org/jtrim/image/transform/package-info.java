39039bf6dff7a6f5802cacccbc5514d2f2f829a7
Attila Kelemen <attila.kelemen85@gmail.com>
2013-06-12 23:49:45 +0200

    Updated the package-info of "org.jtrim.image.transform"

--- jtrim-gui/src/main/java/org/jtrim/image/transform/package-info.java
+++ jtrim-gui/src/main/java/org/jtrim/image/transform/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Contains various image transformations, mostly to support
- * {@link org.jtrim.swing.component.AsyncImageDisplay}.
+ * {@link org.jtrim.swing.component.TransformedImageDisplay}.
  */
 package org.jtrim.image.transform;
