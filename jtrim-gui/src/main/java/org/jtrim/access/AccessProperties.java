47fbced22e795f938c80de0d2d98df013d3fe429
Attila Kelemen <attila.kelemen85@gmail.com>
2013-07-12 23:42:17 +0200

    Added static utility methods to create boolean properties tracking the availability of rights in an AccessManager.

---
+++ jtrim-gui/src/main/java/org/jtrim/access/AccessProperties.java
@@ -0,0 +1,255 @@
+package org.jtrim.access;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import org.jtrim.event.ListenerRef;
+import org.jtrim.property.PropertySource;
+import org.jtrim.utils.ExceptionHelper;
+
+/**
+ * Defines static utility methods to track the availability of rights of an
+ * {@link AccessManager} as a {@link PropertySource} (with {@code Boolean} value).
+ * <P>
+ * Note that these properties are not intended for synchronization purposes but
+ * they can be used to enable and disable GUI components or change their state
+ * otherwise.
+ *
+ * @see org.jtrim.property.swing.AutoDisplayState
+ *
+ * @author Kelemen Attila
+ */
+public final class AccessProperties {
+    /**
+     * Returns a property which tracks if the given {@code AccessRequest} is
+     * available or not. That is, if it can be acquired or not from the
+     * specified {@code AccessManager}. The returned property is not intended to
+     * be used as a true synchronization utility, instead it can be used to
+     * change the state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param request the {@code AccessRequest} whose availability is to be
+     *   tracked. This argument cannot be {@code null}.
+     * @return a property which tracks if the given read and write rights are
+     *   available or not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackRequestAvailable(
+            AccessManager<?, ? super RightType> accessManager,
+            AccessRequest<?, ? extends RightType> request) {
+        return trackRightsAvailable(accessManager, request.getReadRights(), request.getWriteRights());
+    }
+
+    /**
+     * Returns a property which tracks if the given read rights are available
+     * or not. That is, if they can be acquired or not from the specified
+     * {@code AccessManager}. The returned property is not intended to be used
+     * as a true synchronization utility, instead it can be used to change the
+     * state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param readRights the rights requiring read access. This argument
+     *   cannot be {@code null} (but can be an empty collection) and none of its
+     *   elements is allowed to be {@code null}.
+     * @return a property which tracks if the given read rights are available
+     *   or not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackReadRightsAvailable(
+            AccessManager<?, ? super RightType> accessManager,
+            Collection<? extends RightType> readRights) {
+        return trackRightsAvailable(accessManager, readRights, Collections.<RightType>emptySet());
+    }
+
+    /**
+     * Returns a property which tracks if the given read right is available or
+     * not. That is, if it can be acquired or not from the specified
+     * {@code AccessManager}. The returned property is not intended to be used
+     * as a true synchronization utility, instead it can be used to change
+     * the state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param readRight the right requiring read access. This argument cannot
+     *   be {@code null}
+     * @return a property which tracks if the given read right is available or
+     *   not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackReadRightAvailable(
+            AccessManager<?, ? super RightType> accessManager,
+            RightType readRight) {
+        return trackRightsAvailable(accessManager,
+                Collections.singleton(readRight),
+                Collections.<RightType>emptySet());
+    }
+
+    /**
+     * Returns a property which tracks if the given write rights are available
+     * or not. That is, if they can be acquired or not from the specified
+     * {@code AccessManager}. The returned property is not intended to be used
+     * as a true synchronization utility, instead it can be used to change the
+     * state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param writeRights the rights requiring write access. This argument
+     *   cannot be {@code null} (but can be an empty collection) and none of its
+     *   elements is allowed to be {@code null}.
+     * @return a property which tracks if the given write rights are available
+     *   or not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackWriteRightsAvailable(
+            AccessManager<?, ? super RightType> accessManager,
+            Collection<? extends RightType> writeRights) {
+        return trackRightsAvailable(accessManager, Collections.<RightType>emptySet(), writeRights);
+    }
+
+    /**
+     * Returns a property which tracks if the given write right is available or
+     * not. That is, if it can be acquired or not from the specified
+     * {@code AccessManager}. The returned property is not intended to be used
+     * as a true synchronization utility, instead it can be used to change
+     * the state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param writeRight the right requiring write access. This argument cannot
+     *   be {@code null}
+     * @return a property which tracks if the given write right is available or
+     *   not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackWriteRightAvailable(
+            AccessManager<?, ? super RightType> accessManager,
+            RightType writeRight) {
+        return trackRightsAvailable(accessManager,
+                Collections.<RightType>emptySet(),
+                Collections.singleton(writeRight));
+    }
+
+    /**
+     * Returns a property which tracks if the given read and write rights
+     * are available or not. That is, if they can be acquired or not from the
+     * specified {@code AccessManager}. The returned property is not intended to
+     * be used as a true synchronization utility, instead it can be used to
+     * change the state of GUI components based on the availability of rights.
+     * <P>
+     * Note that listeners registered with the returned property are not
+     * necessarily invoked on the Event Dispatch Thread.
+     *
+     * @param <RightType> the type of the rights managed by the specified
+     *   {@code AccessManager}
+     * @param accessManager the {@code AccessManager} managing the availability
+     *   of rights. This argument cannot be {@code null}.
+     * @param readRights the rights requiring read access. This argument cannot
+     *   be {@code null} (but can be an empty collection) and none of its
+     *   elements is allowed to be {@code null}.
+     * @param writeRights the rights requiring write access. This argument
+     *   cannot be {@code null} (but can be an empty collection) and none of its
+     *   elements is allowed to be {@code null}.
+     * @return a property which tracks if the given read and write rights are
+     *   available or not. This method never returns {@code null}.
+     *
+     * @throws NullPointerException thrown if any of the arguments is
+     *   {@code null}
+     */
+    public static <RightType> PropertySource<Boolean> trackRightsAvailable(
+            final AccessManager<?, ? super RightType> accessManager,
+            Collection<? extends RightType> readRights,
+            Collection<? extends RightType> writeRights) {
+        // The bridge method is created, so that we can provide a more generic
+        // method call avoiding syntax errors.
+        return trackRightsAvailableBridge(accessManager, readRights, writeRights);
+    }
+
+    private static <IDType, RightType> PropertySource<Boolean> trackRightsAvailableBridge(
+            final AccessManager<IDType, RightType> accessManager,
+            Collection<? extends RightType> readRights,
+            Collection<? extends RightType> writeRights) {
+        return new RightTrackerPropertySource<>(accessManager, readRights, writeRights);
+    }
+
+    private AccessProperties() {
+        throw new AssertionError();
+    }
+
+    private static final class RightTrackerPropertySource<IDType, RightType> implements PropertySource<Boolean> {
+        private final AccessManager<IDType, RightType> accessManager;
+        private final Collection<RightType> readRights;
+        private final Collection<RightType> writeRights;
+
+        public RightTrackerPropertySource(
+                AccessManager<IDType, RightType> accessManager,
+                Collection<? extends RightType> readRights,
+                Collection<? extends RightType> writeRights) {
+
+            ExceptionHelper.checkNotNullArgument(accessManager, "accessManager");
+
+            this.accessManager = accessManager;
+            this.readRights = new ArrayList<>(readRights);
+            this.writeRights = new ArrayList<>(writeRights);
+
+            ExceptionHelper.checkNotNullElements(this.readRights, "readRights");
+            ExceptionHelper.checkNotNullElements(this.writeRights, "writeRights");
+        }
+
+        @Override
+        public Boolean getValue() {
+            return accessManager.isAvailable(readRights, writeRights);
+        }
+
+        @Override
+        public ListenerRef addChangeListener(final Runnable listener) {
+            ExceptionHelper.checkNotNullArgument(listener, "listener");
+
+            return accessManager.addAccessChangeListener(new AccessChangeListener<IDType, RightType>() {
+                @Override
+                public void onChangeAccess(
+                        AccessRequest<? extends IDType, ? extends RightType> request,
+                        boolean acquired) {
+
+                    listener.run();
+                }
+            });
+        }
+    }
+}
---
+++ jtrim-gui/src/test/java/org/jtrim/access/AccessPropertiesTest.java
@@ -0,0 +1,280 @@
+
+package org.jtrim.access;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.jtrim.concurrent.SyncTaskExecutor;
+import org.jtrim.event.ListenerRef;
+import org.jtrim.property.PropertySource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kelemen Attila
+ */
+public class AccessPropertiesTest {
+    private HierarchicalAccessManager<String> accessManager;
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+         accessManager = new HierarchicalAccessManager<>(SyncTaskExecutor.getSimpleExecutor());
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private static Collection<HierarchicalRight> singletonRight(TestRight right) {
+        return Collections.singleton(HierarchicalRight.create(right));
+    }
+
+    private static AccessRequest<String, HierarchicalRight> readRequest(TestRight right) {
+        return AccessRequest.getReadRequest("READ-REQUEST", HierarchicalRight.create(right));
+    }
+
+    private static AccessRequest<String, HierarchicalRight> writeRequest(TestRight right) {
+        return AccessRequest.getWriteRequest("WRITE-REQUEST", HierarchicalRight.create(right));
+    }
+
+    private static AccessRequest<String, HierarchicalRight> readWriteRequest() {
+
+        return new AccessRequest<>("READ-WRITE-REQUEST",
+                singletonRight(TestRight.READ_RIGHT),
+                singletonRight(TestRight.WRITE_RIGHT));
+    }
+
+    private AccessResult<String> acquireReadRight(TestRight right) {
+        return accessManager.tryGetAccess(readRequest(right));
+    }
+
+    private AccessResult<String> acquireWriteRight(TestRight right) {
+        return accessManager.tryGetAccess(writeRequest(right));
+    }
+
+    private void doTestsForTrackingReadAndWriteRights(PropertySource<Boolean> property) {
+        assertTrue(property.getValue());
+
+        TestListener listener = new TestListener();
+        ListenerRef listenerRef = property.addChangeListener(listener);
+        assertTrue(listenerRef.isRegistered());
+
+        assertEquals(0, listener.getAndResetCallCount());
+
+        AccessResult<String> result;
+
+        result = acquireReadRight(TestRight.READ_RIGHT);
+        assertTrue(property.getValue());
+
+        result.release();
+        listener.getAndResetCallCount();
+        assertTrue(property.getValue());
+
+        result = acquireWriteRight(TestRight.READ_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        result = acquireReadRight(TestRight.WRITE_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        result = acquireWriteRight(TestRight.WRITE_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        assertTrue(listenerRef.isRegistered());
+
+        listenerRef.unregister();
+        assertFalse(listenerRef.isRegistered());
+        acquireWriteRight(TestRight.WRITE_RIGHT);
+        assertEquals(0, listener.getAndResetCallCount());
+    }
+
+    /**
+     * Test of trackRightsAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackRightsAvailable() {
+        PropertySource<Boolean> property = AccessProperties.trackRightsAvailable(accessManager,
+                singletonRight(TestRight.READ_RIGHT),
+                singletonRight(TestRight.WRITE_RIGHT));
+        doTestsForTrackingReadAndWriteRights(property);
+    }
+
+
+    /**
+     * Test of trackRequestAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackRequestAvailable() {
+        PropertySource<Boolean> property
+                = AccessProperties.trackRequestAvailable(accessManager, readWriteRequest());
+        doTestsForTrackingReadAndWriteRights(property);
+    }
+
+    private void doTestsForTrackingReadRight(PropertySource<Boolean> property) {
+        assertTrue(property.getValue());
+
+        TestListener listener = new TestListener();
+        ListenerRef listenerRef = property.addChangeListener(listener);
+        assertTrue(listenerRef.isRegistered());
+
+        assertEquals(0, listener.getAndResetCallCount());
+
+        AccessResult<String> result;
+
+        result = acquireReadRight(TestRight.READ_RIGHT);
+        assertTrue(property.getValue());
+
+        result.release();
+        listener.getAndResetCallCount();
+        assertTrue(property.getValue());
+
+        result = acquireWriteRight(TestRight.READ_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        assertTrue(listenerRef.isRegistered());
+
+        listenerRef.unregister();
+        assertFalse(listenerRef.isRegistered());
+        acquireWriteRight(TestRight.READ_RIGHT);
+        assertEquals(0, listener.getAndResetCallCount());
+    }
+
+    /**
+     * Test of trackReadRightsAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackReadRightsAvailable() {
+        PropertySource<Boolean> property
+                = AccessProperties.trackReadRightsAvailable(accessManager, singletonRight(TestRight.READ_RIGHT));
+        doTestsForTrackingReadRight(property);
+    }
+
+    /**
+     * Test of trackReadRightAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackReadRightAvailable() {
+        PropertySource<Boolean> property = AccessProperties.trackReadRightAvailable(
+                accessManager,
+                HierarchicalRight.create(TestRight.READ_RIGHT));
+
+        doTestsForTrackingReadRight(property);
+    }
+
+    private void doTestsForTrackingWriteRight(PropertySource<Boolean> property) {
+        assertTrue(property.getValue());
+
+        TestListener listener = new TestListener();
+        ListenerRef listenerRef = property.addChangeListener(listener);
+        assertTrue(listenerRef.isRegistered());
+
+        assertEquals(0, listener.getAndResetCallCount());
+
+        AccessResult<String> result;
+
+        result = acquireReadRight(TestRight.WRITE_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        result = acquireWriteRight(TestRight.WRITE_RIGHT);
+        listener.verifyCalledAndReset();
+        assertFalse(property.getValue());
+
+        result.release();
+        listener.verifyCalledAndReset();
+        assertTrue(property.getValue());
+
+        assertTrue(listenerRef.isRegistered());
+
+        listenerRef.unregister();
+        assertFalse(listenerRef.isRegistered());
+        acquireWriteRight(TestRight.WRITE_RIGHT);
+        assertEquals(0, listener.getAndResetCallCount());
+    }
+
+    /**
+     * Test of trackWriteRightsAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackWriteRightsAvailable() {
+        PropertySource<Boolean> property
+                = AccessProperties.trackWriteRightsAvailable(accessManager, singletonRight(TestRight.WRITE_RIGHT));
+        doTestsForTrackingWriteRight(property);
+    }
+
+    /**
+     * Test of trackWriteRightAvailable method, of class AccessProperties.
+     */
+    @Test
+    public void testTrackWriteRightAvailable() {
+        PropertySource<Boolean> property
+                = AccessProperties.trackWriteRightAvailable(
+                accessManager,
+                HierarchicalRight.create(TestRight.WRITE_RIGHT));
+
+        doTestsForTrackingWriteRight(property);
+    }
+
+    private enum TestRight {
+        READ_RIGHT,
+        WRITE_RIGHT
+    }
+
+    private static class TestListener implements Runnable {
+        private final AtomicInteger callCount;
+
+        public TestListener() {
+            this.callCount = new AtomicInteger(0);
+        }
+
+        public int getAndResetCallCount() {
+            return callCount.getAndSet(0);
+        }
+
+        public void verifyCalledAndReset() {
+            assertTrue("Listener must have been notified.", getAndResetCallCount() > 0);
+        }
+
+        @Override
+        public void run() {
+            callCount.incrementAndGet();
+        }
+    }
+}
