ea4e15eb13ee54257f5c2e2690e2ab1d6520f6b3
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-11 21:49:13 +0100

    Added some convenience method to TaskExecutionException.

--- jtrim-core/src/main/java/org/jtrim/concurrent/TaskExecutionException.java
+++ jtrim-core/src/main/java/org/jtrim/concurrent/TaskExecutionException.java
@@ -55,4 +55,58 @@ public class TaskExecutionException extends RuntimeException {
         super(message, cause);
         ExceptionHelper.checkNotNullArgument(cause, "cause");
     }
+
+    /**
+     * Throws the cause of this exception if it is an instance of {@link Error}
+     * or {@link RuntimeException}, or throws a {@code RuntimeException}
+     * exception with the cause of this exception as the cause of the thrown
+     * exception.
+     * <P>
+     * Note that this method never returns normally and always throws an
+     * exception. The return value is simply for convenience, so that you
+     * can write:
+     * <P>
+     * {@code throw rethrowCause();}
+     * <P>
+     * This allows the Java compiler to detect that the code after this method
+     * is not reachable.
+     *
+     * @return this method never returns, the return value is provided solely
+     *   for convenience
+     */
+    public final RuntimeException rethrowCause() {
+        throw ExceptionHelper.throwUnchecked(getCause());
+    }
+
+    /**
+     * Throws the cause of this exception if it is an instance of {@link Error}
+     * or {@link RuntimeException} or is of the given type, or throws a
+     * {@code RuntimeException} exception with the cause of this exception as
+     * the cause of the thrown exception.
+     * <P>
+     * Note that this method never returns normally and always throws an
+     * exception. The return value is simply for convenience, so that you
+     * can write:
+     * <P>
+     * {@code throw rethrowCause(MyException.class);}
+     * <P>
+     * This allows the Java compiler to detect that the code after this method
+     * is not reachable.
+     *
+     * @param <T> the type of the checked exception type which might be rethrown
+     *   as is
+     * @param checkedType a type defining a {@code Throwable}, if the cause
+     *   implements this class, it is simply rethrown instead of wrapped. This
+     *   argument cannot be {@code null}. However, if {@code null} is passed for
+     *   this argument, the cause is still rethrown but a
+     *   {@code NullPointerException} will be attached to it as a suppressed
+     *   exception.
+     * @return this method never returns, the return value is provided solely
+     *   for convenience
+     *
+     * @throws T thrown if the given exception is an instance of the given class
+     */
+    public final <T extends Throwable> RuntimeException rethrowCause(Class<? extends T> checkedType) throws T {
+        throw ExceptionHelper.throwChecked(getCause(), checkedType);
+    }
 }
--- jtrim-core/src/test/java/org/jtrim/concurrent/TaskExecutionExceptionTest.java
+++ jtrim-core/src/test/java/org/jtrim/concurrent/TaskExecutionExceptionTest.java
@@ -49,4 +49,100 @@ public class TaskExecutionExceptionTest {
         assertSame(cause, exception.getCause());
         assertEquals(message, exception.getMessage());
     }
+
+    private void rethrow(Throwable cause) {
+        new TaskExecutionException(cause).rethrowCause();
+    }
+
+    private <T extends Throwable> void rethrow(Throwable cause, Class<? extends T> checkedType) throws T {
+        new TaskExecutionException(cause).rethrowCause(checkedType);
+    }
+
+    @Test
+    public void testThrowUncheckedException() {
+        try {
+            rethrow(new TestException());
+            fail("Expected RuntimeException.");
+        } catch (RuntimeException ex) {
+            assertTrue(ex.getCause() instanceof TestException);
+        }
+    }
+
+    @Test(expected = TestRuntimeException.class)
+    public void testThrowUncheckedRuntimException() {
+        rethrow(new TestRuntimeException());
+    }
+
+    @Test(expected = TestError.class)
+    public void testThrowUncheckedError() {
+        rethrow(new TestError());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThrowUncheckedNull() {
+        rethrow(null);
+    }
+
+    @Test
+    public void testThrowCheckedException() throws TestException2 {
+        try {
+            rethrow(new TestException(), TestException2.class);
+            fail("Expected RuntimeException.");
+        } catch (RuntimeException ex) {
+            assertTrue(ex.getCause() instanceof TestException);
+        }
+    }
+
+    @Test(expected = TestException.class)
+    public void testThrowCheckedException2() throws TestException {
+        rethrow(new TestException(), TestException.class);
+    }
+
+    @Test(expected = TestRuntimeException.class)
+    public void testThrowCheckedRuntimException() throws TestException {
+        rethrow(new TestRuntimeException(), TestException.class);
+    }
+
+    @Test(expected = TestError.class)
+    public void testThrowCheckedError() throws TestException {
+        rethrow(new TestError(), TestException.class);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThrowCheckedNull1() throws TestException {
+        rethrow(null, TestException.class);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThrowCheckedNull2() throws Throwable {
+        rethrow(null, null);
+    }
+
+    @Test
+    public void testThrowCheckedNull3() throws Throwable {
+        try {
+            rethrow(new RuntimeException(), null);
+            fail("Expected TestException.");
+        } catch (RuntimeException ex) {
+            Throwable[] suppressed = ex.getSuppressed();
+            assertTrue("Must have suppressed exception", suppressed.length == 1);
+            assertTrue(suppressed[0] instanceof NullPointerException);
+        }
+    }
+
+    private static class TestRuntimeException extends RuntimeException {
+        private static final long serialVersionUID = -7461262938744477494L;
+    }
+
+    private static class TestException extends Exception {
+        private static final long serialVersionUID = 4344550423252703187L;
+    }
+
+    private static class TestException2 extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
+
+    private static class TestError extends Error {
+        private static final long serialVersionUID = -8295518361535183378L;
+    }
 }
