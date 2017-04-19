be55c39abc90eb76f43b0627f76bca51ba05ddbe
Attila Kelemen <attila.kelemen85@gmail.com>
2017-04-20 00:47:44 +0200

    Added javadoc for TaskFactoryGroupConfigurer.

--- jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryGroupConfigurer.java
+++ jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryGroupConfigurer.java
@@ -1,5 +1,26 @@
 package org.jtrim.taskgraph;
 
+/**
+ * Defines the configuration of a task node factory. Implementations
+ * of this interface must be repeatable without any side effect.
+ *
+ * <h3>Thread safety</h3>
+ * The method of this interface must be safely callable concurrently
+ * from multiple threads.
+ *
+ * <h4>Synchronization transparency</h4>
+ * The method of this interface is not required to be <I>synchronization transparent</I>.
+ * However, the method of this interface must expected to be called from any thread.
+ *
+ * @see TaskGraphDefConfigurer
+ */
 public interface TaskFactoryGroupConfigurer {
+    /**
+     * Configures the associated task node factory.
+     *
+     * @param properties the properties of the associated task node factory to
+     *   be configured. This argument may only be used until this method returns.
+     *   This argument cannot be {@code null}.
+     */
     public void setup(TaskFactoryProperties.Builder properties);
 }
