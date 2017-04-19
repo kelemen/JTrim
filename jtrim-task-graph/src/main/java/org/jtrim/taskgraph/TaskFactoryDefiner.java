52883639e407cb99ddf42a62c0ddd89625b1fd4e
Attila Kelemen <attila.kelemen85@gmail.com>
2017-04-19 22:58:11 +0200

    Removed less useful convenience method from TaskFactoryDefiner.

--- jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryDefiner.java
+++ jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryDefiner.java
@@ -10,8 +10,4 @@ public interface TaskFactoryDefiner {
     public default <R, I> void defineSimpleFactory(Class<R> resultType, Class<I> factoryArgType, TaskFactory<R, I> setup) {
         defineFactory(new TaskFactoryKey<>(resultType, factoryArgType), (properties) -> setup);
     }
-
-    public default <R> void defineSimpleConstFactory(Class<R> resultType, TaskFactory<R, Void> setup) {
-        defineFactory(new TaskFactoryKey<>(resultType, Void.class), (properties) -> setup);
-    }
 }
