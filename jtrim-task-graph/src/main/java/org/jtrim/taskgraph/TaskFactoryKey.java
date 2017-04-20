ca6322186b5fdbc8aeb41827eeecff37970ae145
Attila Kelemen <attila.kelemen85@gmail.com>
2017-04-21 00:24:35 +0200

    Removed redundant header from the javadoc of TaskFactoryKey.

--- jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryKey.java
+++ jtrim-task-graph/src/main/java/org/jtrim/taskgraph/TaskFactoryKey.java
@@ -8,7 +8,7 @@ import org.jtrim.utils.ExceptionHelper;
  * task factory argument itself, it also uniquely identifies a particular node
  * in the task execution graph.
  *
- * <h3>Thread safety</h3>
+ *
  * <h3>Thread safety</h3>
  * This class is immutable (with the assumption that the {@link #getKey() custom key} is also immutable,
  * and as such, its methods can be safely accessed from multiple concurrent threads.
