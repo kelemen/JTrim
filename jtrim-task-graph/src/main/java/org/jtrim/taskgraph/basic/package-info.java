b764f5d6c2be110799c8caab55f4aac5189d5e0b
Attila Kelemen <attila.kelemen85@gmail.com>
2017-04-23 01:11:33 +0200

    Added package-info for "org.jtrim.taskgraph.basic".

---
+++ jtrim-task-graph/src/main/java/org/jtrim/taskgraph/basic/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Defines basic implementations of the task graph execution framework.
+ *
+ * @see org.jtrim.taskgraph.basic.TaskExecutionRestrictionStrategies
+ * @see org.jtrim.taskgraph.basic.CollectingTaskGraphDefConfigurer
+ */
+package org.jtrim.taskgraph.basic;
