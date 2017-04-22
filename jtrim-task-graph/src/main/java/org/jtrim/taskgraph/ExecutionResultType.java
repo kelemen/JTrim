020049555dba5d87c78b9cbaaf0d0d93ded80040
Attila Kelemen <attila.kelemen85@gmail.com>
2017-04-22 18:05:50 +0200

    Added javadoc for ExecutionResultType.

--- jtrim-task-graph/src/main/java/org/jtrim/taskgraph/ExecutionResultType.java
+++ jtrim-task-graph/src/main/java/org/jtrim/taskgraph/ExecutionResultType.java
@@ -1,7 +1,27 @@
 package org.jtrim.taskgraph;
 
+/**
+ * Defines the possible general outcomes of a task graph execution.
+ *
+ * @see TaskGraphExecutionResult
+ */
 public enum ExecutionResultType {
+    /**
+     * {@code ERRORED} means that the task graph execution failed due to
+     * an exception being thrown while trying to compute the output of
+     * a task node.
+     */
     ERRORED,
+    /**
+     * {@code CANCELED} means that the task graph execution was canceled
+     * before it could have been fully computed and no other unexpected
+     * exceptions were thrown.
+     */
     CANCELED,
+
+    /**
+     * {@code SUCCESS} means that the task graph execution completed
+     * fully without any exception being raised and without being canceled.
+     */
     SUCCESS;
 }
