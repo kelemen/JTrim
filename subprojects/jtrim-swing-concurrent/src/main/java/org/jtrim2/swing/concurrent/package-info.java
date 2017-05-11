48d6d11b092afdd2eff37363dbaa5572f1492306
Attila Kelemen <attila.kelemen85@gmail.com>
2017-05-11 23:27:57 +0200

    Moved @see org.jtrim2.swing.concurrent.BackgroundTaskExecutor Javadoc entry to the appropriate package.

--- subprojects/jtrim-swing-concurrent/src/main/java/org/jtrim2/swing/concurrent/package-info.java
+++ subprojects/jtrim-swing-concurrent/src/main/java/org/jtrim2/swing/concurrent/package-info.java
@@ -2,7 +2,6 @@
  * Contains classes and interface to help with executing tasks of Swing
  * applications concurrently.
  *
- * @see org.jtrim2.swing.concurrent.BackgroundTaskExecutor
  * @see org.jtrim2.swing.concurrent.SwingExecutors
  */
 package org.jtrim2.swing.concurrent;
