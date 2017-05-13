a168581c76c0502a26efa1d32f623fa9a02c82ae
Attila Kelemen <attila.kelemen85@gmail.com>
2017-05-13 15:07:42 +0200

    Added tests for SwingQueries.

---
+++ subprojects/jtrim-swing-query/src/main/java/org/jtrim2/swing/concurrent/query/SwingQueries.java
@@ -0,0 +1,28 @@
+package org.jtrim2.swing.concurrent.query;
+
+import org.jtrim2.access.AccessManager;
+import org.jtrim2.swing.concurrent.SwingExecutors;
+import org.jtrim2.ui.concurrent.query.BackgroundDataProvider;
+
+public final class SwingQueries {
+    /**
+     * Creates a {@code BackgroundDataProvider} serving the queried data on the <I>AWT Event Dispatch Thread</I>.
+     *
+     * @param <IDType> the type of the request ID of the underlying access manager
+     * @param <RightType> the type of the rights handled by the underlying access
+     *   manager
+     * @param accessManager the {@code AccessManager} from which access tokens
+     *   are requested to transfer data in their context. This argument cannot
+     *   be {@code null}.
+     * @return a {@code BackgroundDataProvider} serving the queried data on the <I>AWT Event Dispatch Thread</I>.
+     *   This method never returns {@code null}.
+     */
+    public static <IDType, RightType> BackgroundDataProvider<IDType, RightType> getSwingBackgroundDataProvider(
+            AccessManager<IDType, RightType> accessManager) {
+        return new BackgroundDataProvider<>(accessManager, SwingExecutors.getSwingExecutorProvider());
+    }
+
+    private SwingQueries() {
+        throw new AssertionError();
+    }
+}
---
+++ subprojects/jtrim-swing-query/src/test/java/org/jtrim2/swing/concurrent/query/SwingQueriesTest.java
@@ -0,0 +1,83 @@
+package org.jtrim2.swing.concurrent.query;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.swing.SwingUtilities;
+import org.jtrim2.access.AccessManager;
+import org.jtrim2.access.AccessRequest;
+import org.jtrim2.access.HierarchicalAccessManager;
+import org.jtrim2.access.HierarchicalRight;
+import org.jtrim2.cancel.Cancellation;
+import org.jtrim2.concurrent.query.AsyncDataLink;
+import org.jtrim2.concurrent.query.AsyncDataListener;
+import org.jtrim2.concurrent.query.AsyncReport;
+import org.jtrim2.concurrent.query.SimpleDataController;
+import org.jtrim2.executor.SyncTaskExecutor;
+import org.jtrim2.testutils.TestUtils;
+import org.jtrim2.testutils.swing.component.GuiTestUtils;
+import org.jtrim2.ui.concurrent.query.BackgroundDataProvider;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class SwingQueriesTest {
+    @Test
+    public void testUtility() {
+        TestUtils.testUtilityClass(SwingQueries.class);
+    }
+
+    @Test
+    public void testGetSwingBackgroundDataProvider() {
+        // This is just a basic test that the factory method is not borken.
+        // There are better tests in BackgroundDataProviderTest
+
+        AccessManager<String, HierarchicalRight> manager
+                = new HierarchicalAccessManager<>(SyncTaskExecutor.getSimpleExecutor());
+        BackgroundDataProvider<String, HierarchicalRight> dataProvider
+                = SwingQueries.getSwingBackgroundDataProvider(manager);
+
+        AccessRequest<String, HierarchicalRight> request
+                = AccessRequest.getWriteRequest("", HierarchicalRight.create("RIGHT"));
+
+        AsyncReport report = AsyncReport.getReport(new Exception("TestException"), false);
+        AsyncDataLink<String> link = dataProvider.createLink(request, (cancelToken, dataListener) -> {
+            dataListener.onDataArrive("DATA1");
+            dataListener.onDoneReceive(report);
+
+            return new SimpleDataController();
+        });
+
+        AtomicReference<AsyncReport> reportRef = new AtomicReference<>();
+        List<String> receivedData = new ArrayList<>();
+        Runnable wrongThread = mock(Runnable.class);
+        link.getData(Cancellation.UNCANCELABLE_TOKEN, new AsyncDataListener<String>() {
+            private void checkThread() {
+                if (!SwingUtilities.isEventDispatchThread()) {
+                    wrongThread.run();
+                }
+            }
+
+            @Override
+            public void onDataArrive(String data) {
+                receivedData.add(data);
+                checkThread();
+            }
+
+            @Override
+            public void onDoneReceive(AsyncReport report) {
+                reportRef.set(report);
+                checkThread();
+            }
+        });
+
+        GuiTestUtils.waitAllSwingEvents();
+
+        assertEquals(Arrays.asList("DATA1"), receivedData);
+        assertSame(report, reportRef.get());
+
+        verifyZeroInteractions(wrongThread);
+    }
+}
