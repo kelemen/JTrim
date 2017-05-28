ead1b21a1bd1ff52ee7f9a5cf8aaf25ad43cd8f0
Attila Kelemen <attila.kelemen85@gmail.com>
2017-05-28 23:55:59 +0200

    Created a convenience base class: AbstractExecutorTests not exporting any test methods.

---
+++ subprojects-internal/jtrim-concurrent-tests/src/main/java/org/jtrim2/testutils/executor/AbstractExecutorTests.java
@@ -0,0 +1,73 @@
+package org.jtrim2.testutils.executor;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import org.jtrim2.executor.CancelableFunction;
+import org.jtrim2.executor.CancelableTask;
+import org.jtrim2.executor.TaskExecutor;
+import org.jtrim2.executor.TaskExecutorService;
+import org.jtrim2.testutils.JTrimTests;
+import org.jtrim2.testutils.UnsafeFunction;
+
+public abstract class AbstractExecutorTests<E extends TaskExecutor> extends JTrimTests<TestExecutorFactory<E>> {
+    public AbstractExecutorTests(Collection<? extends TestExecutorFactory<E>> factories) {
+        super(factories);
+    }
+
+    public static <E extends TaskExecutor, W extends TaskExecutorService> TestExecutorFactory<E> wrappedExecutor(
+            Supplier<? extends W> wrappedFactory,
+            Function<? super W, ? extends E> wrapperFactory) {
+        Objects.requireNonNull(wrappedFactory, "wrapped");
+        Objects.requireNonNull(wrapperFactory, "wrapperFactory");
+
+        return new TestExecutorFactory<>(() -> {
+            W wrappedExecutor = wrappedFactory.get();
+            E executor = wrapperFactory.apply(wrappedExecutor);
+            return new TestExecutorFactory.ExecutorRef<>(executor, () -> {
+                GenericExecutorServiceTests.shutdownTestExecutor(wrappedExecutor);
+            });
+        });
+    }
+
+    public static <V> MockFunction<V> mockFunction(V result) {
+        return MockFunction.mock(result);
+    }
+
+    public static CancelableTask toTask(MockTask mockTask) {
+        return MockTask.toTask(mockTask);
+    }
+
+    public static <V> CancelableFunction<V> toFunction(MockFunction<V> mockFunction) {
+        return MockFunction.toFunction(mockFunction);
+    }
+
+    public static <V> BiConsumer<V, Throwable> toCleanupTask(MockCleanup mockCleanup) {
+        return MockCleanup.toCleanupTask(mockCleanup);
+    }
+
+    public static <V> V waitResult(CompletionStage<V> future) {
+        return CompletionStages.get(future.toCompletableFuture(), 10, TimeUnit.SECONDS);
+    }
+
+    public static <V> V waitResultWithCallback(CompletionStage<V> future) {
+        CompletableFuture<V> waitableFuture = CompletionStages.toSafeWaitable(future);
+        return CompletionStages.get(waitableFuture, 10, TimeUnit.SECONDS);
+    }
+
+    protected final void testAllCreated(
+            UnsafeFunction<? super E, ? extends AfterTerminate> testMethod) throws Exception {
+
+        testAll((factory) -> {
+            AfterTerminate afterTerminateVerification = factory.runTest(testMethod);
+            if (afterTerminateVerification != null) {
+                afterTerminateVerification.verifyAfterTerminate();
+            }
+        });
+    }
+}
--- subprojects-internal/jtrim-concurrent-tests/src/main/java/org/jtrim2/testutils/executor/GenericExecutorTests.java
+++ subprojects-internal/jtrim-concurrent-tests/src/main/java/org/jtrim2/testutils/executor/GenericExecutorTests.java
@@ -3,15 +3,11 @@ package org.jtrim2.testutils.executor;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import org.jtrim2.cancel.Cancellation;
 import org.jtrim2.cancel.CancellationToken;
 import org.jtrim2.cancel.OperationCanceledException;
@@ -21,10 +17,7 @@ import org.jtrim2.executor.CancelableFunction;
 import org.jtrim2.executor.CancelableTask;
 import org.jtrim2.executor.ContextAwareTaskExecutor;
 import org.jtrim2.executor.TaskExecutor;
-import org.jtrim2.executor.TaskExecutorService;
-import org.jtrim2.testutils.JTrimTests;
 import org.jtrim2.testutils.UnsafeConsumer;
-import org.jtrim2.testutils.UnsafeFunction;
 import org.jtrim2.testutils.UnsafeRunnable;
 import org.jtrim2.testutils.cancel.TestCancellationSource;
 import org.junit.Test;
@@ -35,49 +28,12 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 
-public abstract class GenericExecutorTests<E extends TaskExecutor> extends JTrimTests<TestExecutorFactory<E>> {
+public abstract class GenericExecutorTests<E extends TaskExecutor>
+extends
+        AbstractExecutorTests<E> {
+
     public GenericExecutorTests(Collection<? extends TestExecutorFactory<E>> factories) {
         super(factories);
-    }
-
-    public static <E extends TaskExecutor, W extends TaskExecutorService> TestExecutorFactory<E> wrappedExecutor(
-            Supplier<? extends W> wrappedFactory,
-            Function<? super W, ? extends E> wrapperFactory) {
-        Objects.requireNonNull(wrappedFactory, "wrapped");
-        Objects.requireNonNull(wrapperFactory, "wrapperFactory");
-
-        return new TestExecutorFactory<>(() -> {
-            W wrappedExecutor = wrappedFactory.get();
-            E executor = wrapperFactory.apply(wrappedExecutor);
-            return new TestExecutorFactory.ExecutorRef<>(executor, () -> {
-                GenericExecutorServiceTests.shutdownTestExecutor(wrappedExecutor);
-            });
-        });
-    }
-
-    public static <V> MockFunction<V> mockFunction(V result) {
-        return MockFunction.mock(result);
-    }
-
-    public static CancelableTask toTask(MockTask mockTask) {
-        return MockTask.toTask(mockTask);
-    }
-
-    public static <V> CancelableFunction<V> toFunction(MockFunction<V> mockFunction) {
-        return MockFunction.toFunction(mockFunction);
-    }
-
-    public static <V> BiConsumer<V, Throwable> toCleanupTask(MockCleanup mockCleanup) {
-        return MockCleanup.toCleanupTask(mockCleanup);
-    }
-
-    public static <V> V waitResult(CompletionStage<V> future) {
-        return CompletionStages.get(future.toCompletableFuture(), 10, TimeUnit.SECONDS);
-    }
-
-    public static <V> V waitResultWithCallback(CompletionStage<V> future) {
-        CompletableFuture<V> waitableFuture = CompletionStages.toSafeWaitable(future);
-        return CompletionStages.get(waitableFuture, 10, TimeUnit.SECONDS);
     }
 
     public static void verifyResultOrCanceled(MockCleanup cleanup, Object expectedResult) {
@@ -304,17 +260,6 @@ public abstract class GenericExecutorTests<E extends TaskExecutor> extends JTrim
     @Test(timeout = 10000)
     public final void testCancellationWithCleanups() throws Exception {
         testCleanups(false, (executor) -> { });
-    }
-
-    protected final void testAllCreated(
-            UnsafeFunction<? super E, ? extends AfterTerminate> testMethod) throws Exception {
-
-        testAll((factory) -> {
-            AfterTerminate afterTerminateVerification = factory.runTest(testMethod);
-            if (afterTerminateVerification != null) {
-                afterTerminateVerification.verifyAfterTerminate();
-            }
-        });
     }
 
     private static class TestException extends RuntimeException {
