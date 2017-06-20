cbc465ed156abf95b1ce21d630eb0c68743885b1
Attila Kelemen <attila.kelemen85@gmail.com>
2017-06-20 23:21:46 +0200

    Converted enum in TestObjWithIdentity to a lambda.

--- subprojects/jtrim-property/src/test/java/org/jtrim2/property/TestObjWithIdentity.java
+++ subprojects/jtrim-property/src/test/java/org/jtrim2/property/TestObjWithIdentity.java
@@ -4,7 +4,13 @@ import java.util.Objects;
 import org.jtrim2.collections.EqualityComparator;
 
 final class TestObjWithIdentity {
-    public static final EqualityComparator<TestObjWithIdentity> STR_CMP = StrCmp.INSTANCE;
+    public static final EqualityComparator<TestObjWithIdentity> STR_CMP = (obj1, obj2) -> {
+        if (obj1 == null) return obj2 == null;
+        if (obj2 == null) return false;
+
+        return Objects.equals(obj1.str, obj2.str);
+    };
+
     public static final TestObjWithIdentity EMPTY = new TestObjWithIdentity("");
 
     public final String str;
@@ -16,17 +22,5 @@ final class TestObjWithIdentity {
     @Override
     public String toString() {
         return str;
-    }
-
-    private enum StrCmp implements EqualityComparator<TestObjWithIdentity> {
-        INSTANCE;
-
-        @Override
-        public boolean equals(TestObjWithIdentity obj1, TestObjWithIdentity obj2) {
-            if (obj1 == null) return obj2 == null;
-            if (obj2 == null) return false;
-
-            return Objects.equals(obj1.str, obj2.str);
-        }
     }
 }
