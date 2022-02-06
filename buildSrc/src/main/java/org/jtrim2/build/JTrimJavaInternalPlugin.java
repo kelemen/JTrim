597e1120e944bf4da0b3032bff7b704865b9643b
Attila Kelemen <attila.kelemen85@gmail.com>
2022-02-06 19:40:06 +0100

    build: Disabled javadoc task for internal subprojects. They caused problems due to Mockito making `org.objenesis.ObjenesisHelper` visible to javadoc, but having only a runtime dependency on it.

--- buildSrc/src/main/java/org/jtrim2/build/JTrimJavaInternalPlugin.java
+++ buildSrc/src/main/java/org/jtrim2/build/JTrimJavaInternalPlugin.java
@@ -2,11 +2,16 @@ package org.jtrim2.build;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.javadoc.Javadoc;
 
 public final class JTrimJavaInternalPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         ProjectUtils.applyPlugin(project, JTrimJavaBasePlugin.class);
         new CheckStyleConfigurer(project, "internal").configure();
+
+        project.getTasks().withType(Javadoc.class).configureEach(task -> {
+            task.setEnabled(false);
+        });
     }
 }
